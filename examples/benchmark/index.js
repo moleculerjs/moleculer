ba0b78e64fcd1bdbb02546eb5e7ddc47da42514f
Icebob <mereg.norbert@gmail.com>
2016-12-13 23:20:20 +0100

    improve bench

--- examples/benchmark/index.js
+++ examples/benchmark/index.js
@@ -26,84 +26,81 @@ let broker = new ServiceBroker();
 
 let userService = require("./user.service")(broker);
 
-console.log("---------------------------------------\n"); 
+console.log("---------------------------------------\n");
 
-
-console.log(chalk.white.bold("Benchmark #1: Call actions via service methods\n"));
-
-userService.counter = 0;
-let c = ITERATIONS;
-let startTime = Date.now();
-
-function doWork1(c) {
+function bench1ViaService() {
 	return userService.actions.find()
-	.then(() => { return userService.actions.get({ id: 4});	})
-	.then(() => { return userService.actions.get({ id: 2}); })
-	.then(() => { return userService.actions.find(); })
-	/*.then(() => { 
-		if (c) 
-			doWork1(--c);
-		else {
-			//console.log("Count: ", userService.counter);
-			//console.log(chalk.green.bold(`Done. Time: ${Date.now() - startTime}ms\n\n`));
-
-		} 
-	})*/
-	.catch((err) => { console.warn(err); });
+		.then(() => {
+			return userService.actions.get({
+				id: 4
+			});
+		})
+		.then(() => {
+			return userService.actions.get({
+				id: 2
+			});
+		})
+		.then(() => {
+			return userService.actions.find();
+		})
+		.catch((err) => {
+			console.warn(err);
+		});
 }
-
-//doWork1(ITERATIONS);
-
 
 /*heapdump.writeSnapshot(function(err, filename) {  
   console.log('dump written to', filename);
 });*/
 
 
-
-console.log(chalk.white.bold("Benchmark #1: Call actions via local broker\n"));
-
-userService.counter = 0;
-c = ITERATIONS;
-startTime = Date.now();
-function doWork2(c) {
+function bench1ViaBroker() {
 	return broker.call("users.find")
-	.then(() => { return broker.call("users.get", { id: 4});	})
-	.then(() => { return broker.call("users.get", { id: 2}); })
-	.then(() => { return broker.call("users.find"); })
-	/*.then(() => { 
-		if (c) 
-			doWork2(--c);
-		else {
-			console.log("Count: ", userService.counter);
-			console.log(chalk.green.bold(`Done. Time: ${Date.now() - startTime}ms\n\n`));
-
-		} 
-	})*/
-	.catch((err) => { console.warn(err); });
+		.then(() => {
+			return broker.call("users.get", {
+				id: 4
+			});
+		})
+		.then(() => {
+			return broker.call("users.get", {
+				id: 2
+			});
+		})
+		.then(() => {
+			return broker.call("users.find");
+		})
+		.catch((err) => {
+			console.warn(err);
+		});
 }
-
-//doWork2(ITERATIONS);
 
 let suite = new Benchmark.Suite;
 
-suite.add("Call actions via service methods", { 
+suite
+	.add("Call via service methods", {
+		defer: true,
+		fn(deferred) {
+			return bench1ViaService().then(() => deferred.resolve());
+		}
+	})
+	.add("Call via local broker", {
+		defer: true,
+		fn(deferred) {
+			return bench1ViaBroker().then(() => deferred.resolve());
+		}
+	})
+	.on("cycle", function (event) {
+		let bench = event.target;
+		if (bench.error)
+			console.error(chalk.red.bold(String(bench), bench.error.message, "\n", bench.error.stack || ""));
+		else
+			console.log("››", String(bench));
+	})
+	.on("complete", function () {
+		console.log("---", "\nFastest:", this.filter("fastest").map("name").join(", "), "\nSlowest:", this.filter("slowest").map("name").join(", "));
+	});
+
+console.log(chalk.yellow.bold("Benchmark #1"));
+suite.run({
 	defer: true,
-	fn(deferred) {
-		return doWork1().then(() => deferred.resolve());
-	}
-})
-.add("Call actions via local broker", {
-	defer: true,
-	fn(deferred) {
-		return doWork2().then(() => deferred.resolve());
-	}
-})
-.on("cycle", function(event) {
-	console.log(String(event.target));
-})
-.on("complete", function() {
-	console.log('Fastest is ' + this.filter('fastest').map('name'));
-	console.log(this.toString());
-})
-.run({ defer: true, async: true });
+	async: true
+});
