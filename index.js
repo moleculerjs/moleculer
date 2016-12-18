c15b7b0036c7cb3eed6fb274c2a8d2458c665371
Icebob <mereg.norbert@gmail.com>
2016-12-18 22:25:41 +0100

    add index

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+	Context: require("./src/context"),
+	Service: require("./src/service"),
+	ServiceNode: require("./src/service-node"),
+	ServiceBroker: require("./src/service-broker"),
+	ServiceBus: require("./src/service-bus"),
+
+	Errors: require("./src/errors")
+};
