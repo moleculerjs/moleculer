f1522a8cec32aced73e1265536ac227eaf5b3e6e
Icebob <mereg.norbert@gmail.com>
2016-12-19 23:37:42 +0100

    handle empty response

--- examples/demo2/server1.js
+++ examples/demo2/server1.js
@@ -19,16 +19,22 @@ let broker = new ServiceBroker({
 });
 
 require("../demo1/post.service")(broker);
-
+/*
 let c = 1;
 setInterval(() => {
 	broker.emit("test.data" + c++, { a: 1, b: "String", c});	
 }, 200);
-
+*/
 
 let reqID = 123456;
-setInterval(() => {
+setTimeout(() => {
 	broker.transporter.request(broker.internalNode, reqID++, "posts.find").then(response => {
 		console.log("Response: ", response.length);
 	});
 }, 400);
+
+setInterval(() => {
+	broker.transporter.request(broker.internalNode, reqID++, "posts.get", { id: _.random(10) }).then(response => {
+		console.log("Response: ", response ? response.title : "<Not found>");
+	});
+}, 1000);
--- src/transporters/nats.js
+++ src/transporters/nats.js
@@ -33,11 +33,14 @@ class NatsTransporter extends Transporter {
 			// Subscribe to node requests
 			let reqSubject = [PREFIX, this.nodeID, ">"].join(".");
 			this.client.subscribe(reqSubject, (msg, reply, subject) => {
-				let params = JSON.parse(msg);
-				this.broker.call(subject.slice(reqSubject.length - 1), params).then(res => {
-					console.log("REQUEST RECEIVED", params);
+				let params;
+				if (msg != "")
+					params = JSON.parse(msg);
+				let actionName = subject.slice(reqSubject.length - 1);
+				this.broker.call(actionName, params).then(res => {
+					console.log("REQUEST RECEIVED", actionName, params);
 					let payload = JSON.stringify(res);
-					this.client.publish([PREFIX, "REQ", params.$requestID].join("."), payload);
+					this.client.publish(reply, payload);
 				});
 			});
 		});
@@ -67,14 +70,21 @@ class NatsTransporter extends Transporter {
 
 	request(node, requestID, actionName, params) {
 		return new Promise((resolve) => {
-			let subject = [PREFIX, node.id, actionName].join(".");
-			let sid = this.client.subscribe([PREFIX, "REQ", requestID].join("."), (response) => {
-				resolve(JSON.parse(response));
+			let subSubject = [PREFIX, "REQ", requestID].join(".");
+			let sid = this.client.subscribe(subSubject, (response) => {
+				let payload;
+				if (response != "")
+					resolve(JSON.parse(response));
+				else
+					resolve(null);
+					
 				this.client.unsubscribe(sid);
 			});
 
-			let payload = JSON.stringify(Object.assign({}, params, { $requestID: requestID }));
-			this.client.publish(subject, payload);
+			let pubSubject = [PREFIX, node.id, actionName].join(".");
+			//let payload = JSON.stringify(Object.assign({}, params, { $requestID: requestID }));
+			let payload = JSON.stringify(params);
+			this.client.publish(pubSubject, payload, subSubject);
 		});
 	}
 
