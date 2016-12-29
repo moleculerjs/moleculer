f69daab2506f7449462608a139b70d8ddb27b001
Icebob <mereg.norbert@gmail.com>
2016-12-29 22:20:20 +0100

    add banner

--- src/errors.js
+++ src/errors.js
@@ -1,3 +1,11 @@
+/*
+ * ice-services
+ * Copyright (c) 2016 Norbert Mereg (https://github.com/icebob/ice-services)
+ * MIT Licensed
+ */
+
+"use strict";
+
 /**
  * 'Service not found' Error message
  * 
--- src/transporters/index.js
+++ src/transporters/index.js
@@ -1,3 +1,9 @@
+/*
+ * ice-services
+ * Copyright (c) 2016 Norbert Mereg (https://github.com/icebob/ice-services)
+ * MIT Licensed
+ */
+
 "use strict";
 
 module.exports = {
