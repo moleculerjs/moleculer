60f2cf8c42c49e1ed498488d1d87510773174317
Icebob <mereg.norbert@gmail.com>
2017-01-06 21:51:46 +0100

    minor changes in loadtest

--- examples/loadtest/client.js
+++ examples/loadtest/client.js
@@ -12,26 +12,31 @@ let broker = new ServiceBroker({
 	//logger: console
 });
 
+//broker.loadService(__dirname + "/../math.service");
+
 broker.start();
 
-console.log("Client started. nodeID:", broker.nodeID, ", PID:", process.pid);
+console.log("Client started. nodeID:", broker.nodeID, " PID:", process.pid);
 
 function work() {
 	let payload = { a: _.random(0, 100), b: _.random(0, 100) };
 	broker.call("math.add", payload)
 	.then(res => {
 		//console.info(`${payload.a} + ${payload.b} = ${res}`);
-		process.nextTick(work);
+		setImmediate(work);
 	});		
 }
 
 setTimeout(() => { 
+	let startTime = Date.now();
 	work();
 
 	setInterval(() => {
 		if (broker._callCount > 0) {
-			console.log(broker.nodeID, ":", broker._callCount, " req/s");
+			let rps = broker._callCount / ((Date.now() - startTime) / 1000);
+			console.log(broker.nodeID, ":", rps.toFixed(0), "req/s");
 			broker._callCount = 0;
+			startTime = Date.now();
 		}
 	}, 1000);
 
