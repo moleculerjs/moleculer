2dba245081f205b67f61d5fa36f98d96a51a5237
Icebob <mereg.norbert@gmail.com>
2017-01-08 17:59:56 +0100

    add wrapLogger tests

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -46,6 +46,7 @@ describe("Test utils", () => {
 		expect(utils.isPromise({})).toBeFalsy();
 		expect(utils.isPromise(new Promise(() => {}))).toBeTruthy();
 		expect(utils.isPromise(Promise.resolve())).toBeTruthy();
+		//expect(utils.isPromise(Promise.reject())).toBeTruthy(); // node gives warning
 	});
 });
 
@@ -167,14 +168,23 @@ describe("Test utils.json2String", () => {
 	});
 });
 
+function callLogMethods(logger) {
+	logger.log("log level");
+	logger.info("info level");
+	logger.debug("debug level");
+	logger.warn("warn level");
+	logger.error("error level");	
+}
+
 describe("Test utils.wrapLogger", () => {
-	let con = {
-		info: jest.fn(),
-		error: jest.fn(),
-		warn: jest.fn()
-	};
 
 	it("should create a full logger without moduleName", () => {
+		let con = {
+			info: jest.fn(),
+			error: jest.fn(),
+			warn: jest.fn()
+		};
+		
 		let logger = utils.wrapLogger(con, null, "debug");
 		expect(typeof logger.log).toBe("function");
 		expect(typeof logger.error).toBe("function");
@@ -182,36 +192,25 @@ describe("Test utils.wrapLogger", () => {
 		expect(typeof logger.info).toBe("function");
 		expect(typeof logger.debug).toBe("function");
 
-		logger.log("log level");
-		expect(con.info).toHaveBeenCalledTimes(1);
-		expect(con.info).toHaveBeenCalledWith("log level");
-
-		logger.warn("warn level");
+		callLogMethods(logger);
 		expect(con.warn).toHaveBeenCalledTimes(1);
 		expect(con.warn).toHaveBeenCalledWith("warn level");
 
-		logger.error("error level");
 		expect(con.error).toHaveBeenCalledTimes(1);
 		expect(con.error).toHaveBeenCalledWith("error level");
 
-		logger.info("info level");
-		expect(con.info).toHaveBeenCalledTimes(2);
-		expect(con.info).toHaveBeenCalledWith("info level");
-
-		logger.debug("debug level");
 		expect(con.info).toHaveBeenCalledTimes(3);
 		expect(con.info).toHaveBeenCalledWith("debug level");
+		expect(con.info).toHaveBeenCalledWith("info level");
+		expect(con.info).toHaveBeenCalledWith("log level");
 	});
-});
 
-describe("Test utils.wrapLogger", () => {
-	let con = {
-		info: jest.fn(),
-		error: jest.fn(),
-		warn: jest.fn()
-	};
-
-	it("should create a full logger without moduleName", () => {
+	it("should create a full logger with moduleName", () => {
+		let con = {
+			info: jest.fn(),
+			error: jest.fn(),
+			warn: jest.fn()
+		};
 		let logger = utils.wrapLogger(con, "Module", "debug");
 		expect(typeof logger.log).toBe("function");
 		expect(typeof logger.error).toBe("function");
@@ -219,25 +218,102 @@ describe("Test utils.wrapLogger", () => {
 		expect(typeof logger.info).toBe("function");
 		expect(typeof logger.debug).toBe("function");
 
-		logger.log("log level");
-		expect(con.info).toHaveBeenCalledTimes(1);
-		expect(con.info).toHaveBeenCalledWith("[Module] log level");
+		callLogMethods(logger);
 
-		logger.warn("warn level");
 		expect(con.warn).toHaveBeenCalledTimes(1);
 		expect(con.warn).toHaveBeenCalledWith("[Module] warn level");
 
-		logger.error("error level");
 		expect(con.error).toHaveBeenCalledTimes(1);
 		expect(con.error).toHaveBeenCalledWith("[Module] error level");
 
-		logger.info("info level");
-		expect(con.info).toHaveBeenCalledTimes(2);
-		expect(con.info).toHaveBeenCalledWith("[Module] info level");
-
-		logger.debug("debug level");
 		expect(con.info).toHaveBeenCalledTimes(3);
+		expect(con.info).toHaveBeenCalledWith("[Module] info level");
+		expect(con.info).toHaveBeenCalledWith("[Module] log level");
 		expect(con.info).toHaveBeenCalledWith("[Module] debug level");
-
 	});
+});
+
+describe("Test utils.wrapLogger with logLevels", () => {
+
+	it("should create a filtered-level logger (warn)", () => {
+		let con = {
+			debug: jest.fn(),
+			info: jest.fn(),
+			error: jest.fn(),
+			warn: jest.fn()
+		};
+		let logger = utils.wrapLogger(con, "Module", "warn");
+		expect(typeof logger.log).toBe("function");
+		expect(typeof logger.error).toBe("function");
+		expect(typeof logger.warn).toBe("function");
+		expect(typeof logger.info).toBe("function");
+		expect(typeof logger.debug).toBe("function");
+
+		callLogMethods(logger);
+		expect(con.debug).toHaveBeenCalledTimes(0);
+		expect(con.info).toHaveBeenCalledTimes(0);
+
+		expect(con.warn).toHaveBeenCalledTimes(1);
+		expect(con.warn).toHaveBeenCalledWith("[Module] warn level");
+		expect(con.error).toHaveBeenCalledTimes(1);
+		expect(con.error).toHaveBeenCalledWith("[Module] error level");
+	});	
+
+	it("should create a filtered-level logger (module error)", () => {
+		let con = {
+			debug: jest.fn(),
+			info: jest.fn(),
+			error: jest.fn(),
+			warn: jest.fn()
+		};
+		let logger = utils.wrapLogger(con, "CTX", {
+			"*": "debug",
+			"CTX": "error"
+		});
+
+		callLogMethods(logger);
+		expect(con.debug).toHaveBeenCalledTimes(0);
+		expect(con.info).toHaveBeenCalledTimes(0);
+		expect(con.warn).toHaveBeenCalledTimes(0);
+		expect(con.error).toHaveBeenCalledTimes(1);
+	});		
+
+	it("should create a filtered-level logger ('*' info)", () => {
+		let con = {
+			debug: jest.fn(),
+			info: jest.fn(),
+			error: jest.fn(),
+			warn: jest.fn()
+		};
+		let logger = utils.wrapLogger(con, "SVC", {
+			"*": "info",
+			"CTX": "error"
+		});
+
+		callLogMethods(logger);
+		expect(con.debug).toHaveBeenCalledTimes(0);
+		expect(con.info).toHaveBeenCalledTimes(2);
+		expect(con.warn).toHaveBeenCalledTimes(1);
+		expect(con.error).toHaveBeenCalledTimes(1);
+	});		
+
+	it("should create an empty logger (false)", () => {
+		let con = {
+			debug: jest.fn(),
+			info: jest.fn(),
+			error: jest.fn(),
+			warn: jest.fn()
+		};
+		let logger = utils.wrapLogger(con, "SVC", {
+			"*": "info",
+			"SVC": false
+		});
+
+		callLogMethods(logger);
+		expect(con.debug).toHaveBeenCalledTimes(0);
+		expect(con.info).toHaveBeenCalledTimes(0);
+		expect(con.warn).toHaveBeenCalledTimes(0);
+		expect(con.error).toHaveBeenCalledTimes(0);
+	});		
+
 });
