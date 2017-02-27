7523f9613b63faeb6e7df2a77314a415c48fe1e7
Icebob <mereg.norbert@gmail.com>
2017-02-27 23:00:22 +0100

    refactor balanced list test

--- test/unit/balanced-list.spec.js
+++ test/unit/balanced-list.spec.js
@@ -1,10 +1,11 @@
 "use strict";
-// Unit: OK!
+
 const BalancedList = require("../../src/balanced-list");
 
-describe("Test BalancedList", () => {
+// Unit: OK!
+describe("Test constructor", () => {
 
-	it("test constructor", () => {
+	it("should create instance with default options", () => {
 		let list = new BalancedList();
 		expect(list).toBeDefined();
 		expect(list.list).toBeDefined();
@@ -16,34 +17,42 @@ describe("Test BalancedList", () => {
 		expect(list.hasLocal()).toBeFalsy();
 	});
 
-	it("test constructor with options", () => {
+	it("should create instance with options", () => {
 		let opts = {
 			preferLocal: false,
 			strategy: BalancedList.STRATEGY_RANDOM
 		};
 		let list = new BalancedList(opts);
 		expect(list).toBeDefined();
-		expect(list.list).toBeDefined();
 		expect(list.opts).toEqual({"preferLocal": false, "strategy": BalancedList.STRATEGY_RANDOM});
-		expect(list.counter).toBe(0);
-		expect(list.count()).toBe(0);
-
-		expect(list.get()).toBeNull();
-		expect(list.hasLocal()).toBeFalsy();
 	});
 
-	it("test get with remote & preferLocal", () => {
-		let obj1 = { a: 1 };
-		let obj2 = { b: 2 };
-		let obj3 = { c: 3 };
-		let list = new BalancedList();
+});
+
+describe("Test Add & get methods with preferLocal = true", () => {
+	let obj1 = { a: 1 };
+	let obj2 = { b: 2 };
+	let obj3 = { c: 3 };
+	let list = new BalancedList();
+
+	it("should add items and not found local item", () => {
 		list.add(obj1, "node1"); // remote
 		list.add(obj2, "node2"); // remote
+
+		expect(list.count()).toBe(2);
+		expect(list.hasLocal()).toBeFalsy();
+		expect(list.counter).toBe(0);
+	});
+
+	it("should add & found local item", () => {
 		list.add(obj3); // local
 
 		expect(list.count()).toBe(3);
 		expect(list.hasLocal()).toBeTruthy();
 		expect(list.counter).toBe(0);
+	});
+
+	it("should return the local item", () => {
 		let item = list.get();
 		expect(item.local).toBeTruthy();
 		expect(item.nodeID).toBeUndefined();
@@ -51,13 +60,17 @@ describe("Test BalancedList", () => {
 		expect(list.counter).toBe(0);
 		expect(list.get().data).toBe(obj3);
 		expect(list.counter).toBe(0);
+	});
 
-		item = list.getLocalItem();
+	it("should found the local item", () => {
+		let item = list.getLocalItem();
 		expect(item.local).toBeTruthy();
 		expect(item.nodeID).toBeUndefined();
 		expect(item.data).toBe(obj3);
+	});
 		
 		
+	it("remove local item and return the remote items with round-robin", () => {
 		list.remove(obj3);
 		expect(list.count()).toBe(2);
 		expect(list.get().data).toBe(obj1);
@@ -68,28 +81,38 @@ describe("Test BalancedList", () => {
 		expect(list.counter).toBe(1);
 		expect(list.hasLocal()).toBeFalsy();
 	
-		item = list.getLocalItem();
+		let item = list.getLocalItem();
 		expect(item).toBeUndefined();
-
-		list.removeByNode("no-id");
-		expect(list.count()).toBe(2);
-
-		list.removeByNode("node1");
-		expect(list.count()).toBe(1);
-	
 	});
 
-	it("test get with remote & NOT preferLocal", () => {
-		let obj1 = { a: 1};
-		let obj2 = { b: 2};
-		let obj3 = { c: 3};
-		let list = new BalancedList({ preferLocal: false});
+	it("remove a non-exist node item", () => {
+		list.removeByNode("no-id");
+		expect(list.count()).toBe(2);
+	});
+
+	it("remove an item by node", () => {
+		list.removeByNode("node1");
+		expect(list.count()).toBe(1);
+	});
+});
+
+describe("Test Add & get methods with preferLocal = true", () => {
+	let obj1 = { a: 1 };
+	let obj2 = { b: 2 };
+	let obj3 = { c: 3 };
+	let list = new BalancedList({ preferLocal: false});
+
+	it("should add items and found local item", () => {
 		list.add(obj1, "node1");
 		list.add(obj2, "node2");
 		list.add(obj3); // local
-		expect(list.hasLocal()).toBeTruthy();
 
+		expect(list.count()).toBe(3);
+		expect(list.hasLocal()).toBeTruthy();
 		expect(list.counter).toBe(0);
+	});
+
+	it("should step items with round-robin", () => {
 		let item = list.get();
 		expect(item.local).toBeFalsy();
 		expect(item.nodeID).toBe("node1");
@@ -105,38 +128,16 @@ describe("Test BalancedList", () => {
 		expect(list.counter).toBe(1);
 		expect(list.get().data).toBe(obj3);
 		expect(list.counter).toBe(2);
-	
 	});
 
+});
 
-	it("test add & get with local", () => {
-		let obj1 = {};
-		let obj2 = {};
-		let list = new BalancedList();
-		list.add(obj1);
-		list.add(obj2);
-		expect(list.counter).toBe(0);
-		let item = list.get();
-		expect(item.local).toBeTruthy();
-		expect(item.nodeID).toBeUndefined();
-		expect(item.data).toBe(obj1);
-		expect(list.counter).toBe(0);
-		expect(list.get().data).toBe(obj1);
-		expect(list.counter).toBe(0);
-		expect(list.hasLocal()).toBeTruthy();
-		
-		list.remove(obj1);
-		expect(list.get().data).toBe(obj2);
-		expect(list.counter).toBe(0);
-		expect(list.get().data).toBe(obj2);
-		expect(list.counter).toBe(0);
-	
-	});
+describe("Test getData method", () => {
+	let obj1 = { a: 1};
+	let obj2 = { b: 5};
+	let list = new BalancedList({ preferLocal: false });
 
 	it("test getData", () => {
-		let obj1 = { a: 1};
-		let obj2 = { b: 5};
-		let list = new BalancedList({ preferLocal: false });
 		list.add(obj1);
 		list.add(obj2);
 		let o = list.getData();
