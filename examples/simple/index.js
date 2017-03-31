d0e0e24651ee76b01f0bfa9316fba91da22ded3d
Icebob <mereg.norbert@gmail.com>
2017-03-31 22:30:17 +0200

    fix simple example log

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -9,19 +9,19 @@ let broker = new ServiceBroker({ logger: console });
 broker.loadService(__dirname + "/../math.service.js");
 
 // Call actions
-broker.call("math.add", { a: 5, b: 3 }).then(res => broker.logger.log("  5 + 3 =", res))
+broker.call("math.add", { a: 5, b: 3 }).then(res => broker.logger.info("  5 + 3 =", res))
 .then(() => {
-	return broker.call("math.sub", { a: 9, b: 2 }).then(res => broker.logger.log("  9 - 2 =", res));
+	return broker.call("math.sub", { a: 9, b: 2 }).then(res => broker.logger.info("  9 - 2 =", res));
 })
 .then(() => {
-	return broker.call("math.mult", { a: 3, b: 4 }).then(res => broker.logger.log("  3 * 4 =", res));
+	return broker.call("math.mult", { a: 3, b: 4 }).then(res => broker.logger.info("  3 * 4 =", res));
 })
 .then(() => {
-	return broker.call("math.div", { a: 8, b: 4 }).then(res => broker.logger.log("  8 / 4 =", res));
+	return broker.call("math.div", { a: 8, b: 4 }).then(res => broker.logger.info("  8 / 4 =", res));
 })
 .then(() => {
 	// Divide by zero!
-	return broker.call("math.div", { a: 5, b: 0 }).then(res => broker.logger.log("  5 / 0 =", res));
+	return broker.call("math.div", { a: 5, b: 0 }).then(res => broker.logger.info("  5 / 0 =", res));
 })
 .catch(err => {
 	broker.logger.error(`Error occured when '${err.ctx.action.name}' action called! Message:`, err.message, " Params:", err.ctx.params);
