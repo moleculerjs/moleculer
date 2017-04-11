58b90cfcc58ee704a56139ecaf4251530d9b6bc6
Icebob <mereg.norbert@gmail.com>
2017-04-11 11:47:50 +0200

    change error codes

--- src/service-broker.js
+++ src/service-broker.js
@@ -703,11 +703,13 @@ class ServiceBroker {
 					return this.call(actionName, params, opts);
 				}
 
-				// Set node status to unavailable
-				// TODO: check are there any other node for this action. If not, don't set unavailable
-				// TODO: don't use this nodeID, because not guaranteed that this node broken. It might call other nodes. Get the thrown nodeID from error.
-				this.nodeUnavailable(nodeID);
 			}
+
+			// Set node status to unavailable
+			// TODO: check there are any other nodes to this action. If not, don't set unavailable
+			// TODO: don't use this nodeID, because not guaranteed that this node broken. It might call other nodes. Get the thrown nodeID from error.
+			if (err.code >= 500)
+				this.nodeUnavailable(nodeID);
 
 			// Need it? this.logger.error("Action request error!", err);
 
