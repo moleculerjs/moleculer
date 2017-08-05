a343c5c19cde7071f7006b98fb3d67479e15af0c
Icebob <mereg.norbert@gmail.com>
2017-08-05 23:28:59 +0200

    test logging with bunyan

--- dev/logger.js
+++ dev/logger.js
@@ -1,8 +1,16 @@
 "use strict";
 
 const ServiceBroker = require("../src/service-broker");
-
+/*
 const logger = require("pino")({ level: "info" });
+
+const broker = new ServiceBroker({ logger });
+
+*/
+
+const bunyan = require("bunyan");
+
+const logger = bunyan.createLogger({ name: "my-broker" });
 
 const broker = new ServiceBroker({ logger });
 
