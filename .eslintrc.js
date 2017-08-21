c4f4399344ba41b0288ed4365610c50023ceb503
rheza <rheza.satria.ta@gmail.com>
2017-08-21 23:11:17 +0700

    fix(style): change space to tab

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,50 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es6": true,
-        "jquery": false,
-        "jest": true,
-        "jasmine": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:security/recommended"
-    ],
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "plugins": [
-        "node",
-        "promise",
-        "security"
-    ],
-    "rules": {
-        "indent": [
-            "warn",
-            "tab",
-            { SwitchCase: 1 }
-        ],
-        "quotes": [
-            "warn",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-var": [
-            "error"
-        ],
-        "no-console": [
-            "error"
-        ],
-        "no-unused-vars": [
-            "warn"
-        ],
-        "no-trailing-spaces": [
-            "error"
-        ],
+	"env": {
+		"node": true,
+		"commonjs": true,
+		"es6": true,
+		"jquery": false,
+		"jest": true,
+		"jasmine": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:security/recommended"
+	],
+	"parserOptions": {
+		"sourceType": "module"
+	},
+	"plugins": [
+		"node",
+		"promise",
+		"security"
+	],
+	"rules": {
+		"indent": [
+			"warn",
+			"tab",
+			{ SwitchCase: 1 }
+		],
+		"quotes": [
+			"warn",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"no-var": [
+			"error"
+		],
+		"no-console": [
+			"error"
+		],
+		"no-unused-vars": [
+			"warn"
+		],
+		"no-trailing-spaces": [
+			"error"
+		],
 		"linebreak-style": [
 			"error",
 			"unix"
@@ -52,7 +52,7 @@ module.exports = {
 		"no-alert": 0,
 		"no-shadow": 0,
 		"security/detect-object-injection": ["off"],
-        "no-process-exit": ["off"],
-        "node/no-unpublished-require": 0
-    }
+		"no-process-exit": ["off"],
+		"node/no-unpublished-require": 0
+	}
 };
