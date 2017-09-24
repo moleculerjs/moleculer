05a49378ed7df0ba13caa4f0e99f8a1f676afb3a
Icebob <mereg.norbert@gmail.com>
2017-09-24 13:58:49 +0200

    add purge code for integration tests

--- test/integration/amqp/broadcast.spc.js
+++ test/integration/amqp/broadcast.spc.js
@@ -3,6 +3,7 @@
 const Promise = require("bluebird");
 const { ServiceBroker } = require("../../..");
 let { extendExpect, protectReject } = require("../../unit/utils");
+const purge = require("./purge");
 
 extendExpect(expect);
 
@@ -32,121 +33,158 @@ function createNode(name, disableBalancer = false) {
 	return broker;
 }
 
-describe("Test AMQPTransporter event broadcast with built-in balancer", () => {
+describe("Test AMQPTransporter", () => {
 
-	const pub = createNode("pub");
-	const sub1 = createNode("sub1");
-	const sub2 = createNode("sub2");
-	const sub3 = createNode("sub3");
+	beforeAll(() => purge(purgeList));
+	afterAll(() => purge(purgeList));
 
-	beforeEach(() => {
-		FLOW = [];
-		Promise.delay(1000).then(() => sub3.start());
+	describe("Test AMQPTransporter event broadcast with built-in balancer", () => {
 
-		return Promise.all([
-			pub.start(),
-			sub1.start(),
-			sub2.start()
-		]);
-	});
+		const pub = createNode("pub");
+		const sub1 = createNode("sub1");
+		const sub2 = createNode("sub2");
+		const sub3 = createNode("sub3");
 
-	afterEach(() => Promise.all([
-		pub.stop(),
-		sub1.stop(),
-		sub2.stop(),
-		sub3.stop()
-	]));
+		beforeEach(() => {
+			FLOW = [];
+			Promise.delay(1000).then(() => sub3.start());
 
-	it("Should send an event to all subscribed nodes.", () => {
-		pub.broadcast("hello.world", { testing: true });
-
-		return Promise.delay(2000).catch(protectReject).then(() => {
-			expect(FLOW).toHaveLength(3);
-			expect(FLOW).toEqual(expect.arrayContaining([
-				"pub",
-				"sub1",
-				"sub2"
-			]));
-		});
-	}, 10000);
-});
-
-
-describe("Test AMQPTransporter event broadcast with DISABLED built-in balancer", () => {
-
-	const pub = createNode("pub", true);
-	const sub1 = createNode("sub1", true);
-	const sub2 = createNode("sub2", true);
-	const sub3 = createNode("sub3", true);
-
-	beforeEach(() => {
-		FLOW = [];
-		Promise.delay(1000).catch(protectReject).then(() => sub3.start());
-
-		return Promise.all([
-			pub.start(),
-			sub1.start(),
-			sub2.start()
-		]);
-	});
-
-	afterEach(() => Promise.all([
-		pub.stop(),
-		sub1.stop(),
-		sub2.stop(),
-		sub3.stop()
-	]));
-
-	it("Should send an event to all subscribed nodes.", () => {
-		pub.broadcast("hello.world", { testing: true });
-
-		return Promise.delay(2000).catch(protectReject).then(() => {
-			expect(FLOW).toHaveLength(3);
-			expect(FLOW).toEqual(expect.arrayContaining([
-				"pub",
-				"sub1",
-				"sub2"
-			]));
-		});
-	}, 10000);
-});
-
-describe("Test AMQPTransporter event broadcastLocal with built-in balancer", () => {
-
-	const pub = createNode("pub");
-	const sub1 = createNode("sub1");
-	const sub2 = createNode("sub2");
-	const sub3 = createNode("sub3");
-
-	beforeEach(() => {
-		FLOW = [];
-		Promise.delay(1000).then(() => sub3.start());
-
-		return Promise.all([
-			pub.start(),
-			sub1.start(),
-			sub2.start()
-		]);
-	});
-
-	afterEach(() => Promise.all([
-		pub.stop(),
-		sub1.stop(),
-		sub2.stop(),
-		sub3.stop()
-	]));
-
-	it("Should send an event to local services.", () => {
-		for(let i = 0; i < 3; i++)
-			pub.broadcastLocal("hello.world", { testing: true });
-
-		return Promise.delay(2000).catch(protectReject).then(() => {
-			// console.log(FLOW);
-			expect(FLOW).toEqual([
-				"pub",
-				"pub",
-				"pub"
+			return Promise.all([
+				pub.start(),
+				sub1.start(),
+				sub2.start()
 			]);
 		});
-	}, 10000);
+
+		afterEach(() => Promise.all([
+			pub.stop(),
+			sub1.stop(),
+			sub2.stop(),
+			sub3.stop()
+		]));
+
+		it("Should send an event to all subscribed nodes.", () => {
+			pub.broadcast("hello.world", { testing: true });
+
+			return Promise.delay(2000).catch(protectReject).then(() => {
+				expect(FLOW).toHaveLength(3);
+				expect(FLOW).toEqual(expect.arrayContaining([
+					"pub",
+					"sub1",
+					"sub2"
+				]));
+			});
+		}, 10000);
+	});
+
+
+	describe("Test AMQPTransporter event broadcast with DISABLED built-in balancer", () => {
+
+		const pub = createNode("pub", true);
+		const sub1 = createNode("sub1", true);
+		const sub2 = createNode("sub2", true);
+		const sub3 = createNode("sub3", true);
+
+		beforeEach(() => {
+			FLOW = [];
+			Promise.delay(1000).catch(protectReject).then(() => sub3.start());
+
+			return Promise.all([
+				pub.start(),
+				sub1.start(),
+				sub2.start()
+			]);
+		});
+
+		afterEach(() => Promise.all([
+			pub.stop(),
+			sub1.stop(),
+			sub2.stop(),
+			sub3.stop()
+		]));
+
+		it("Should send an event to all subscribed nodes.", () => {
+			pub.broadcast("hello.world", { testing: true });
+
+			return Promise.delay(2000).catch(protectReject).then(() => {
+				expect(FLOW).toHaveLength(3);
+				expect(FLOW).toEqual(expect.arrayContaining([
+					"pub",
+					"sub1",
+					"sub2"
+				]));
+			});
+		}, 10000);
+	});
+
+	describe("Test AMQPTransporter event broadcastLocal with built-in balancer", () => {
+
+		const pub = createNode("pub");
+		const sub1 = createNode("sub1");
+		const sub2 = createNode("sub2");
+		const sub3 = createNode("sub3");
+
+		beforeEach(() => {
+			FLOW = [];
+			Promise.delay(1000).then(() => sub3.start());
+
+			return Promise.all([
+				pub.start(),
+				sub1.start(),
+				sub2.start()
+			]);
+		});
+
+		afterEach(() => Promise.all([
+			pub.stop(),
+			sub1.stop(),
+			sub2.stop(),
+			sub3.stop()
+		]));
+
+		it("Should send an event to local services.", () => {
+			for(let i = 0; i < 3; i++)
+				pub.broadcastLocal("hello.world", { testing: true });
+
+			return Promise.delay(2000).catch(protectReject).then(() => {
+				// console.log(FLOW);
+				expect(FLOW).toEqual([
+					"pub",
+					"pub",
+					"pub"
+				]);
+			});
+		}, 10000);
+	});
+
 });
+
+const purgeList = {
+	queues: [
+		"MOL-test-broadcast.EVENTB.pub.hello.world",
+		"MOL-test-broadcast.EVENTB.sub1.hello.world",
+		"MOL-test-broadcast.EVENTB.sub2.hello.world",
+		"MOL-test-broadcast.EVENTB.sub3.hello.world",
+		"MOL-test-broadcast.REQ.event-pub",
+		"MOL-test-broadcast.REQ.event-sub1",
+		"MOL-test-broadcast.REQ.event-sub2",
+		"MOL-test-broadcast.REQ.event-sub3",
+		"MOL-test-broadcast.REQB.$node.actions",
+		"MOL-test-broadcast.REQB.$node.events",
+		"MOL-test-broadcast.REQB.$node.health",
+		"MOL-test-broadcast.REQB.$node.list",
+		"MOL-test-broadcast.REQB.$node.services",
+		"MOL-test-broadcast.RES.event-pub",
+		"MOL-test-broadcast.RES.event-sub1",
+		"MOL-test-broadcast.RES.event-sub2",
+		"MOL-test-broadcast.RES.event-sub3",
+	],
+
+	exchanges: [
+		"MOL-test-broadcast.DISCONNECT",
+		"MOL-test-broadcast.DISCOVER",
+		"MOL-test-broadcast.HEARTBEAT",
+		"MOL-test-broadcast.INFO",
+		"MOL-test-broadcast.PING",
+	]
+};
--- test/integration/amqp/rpc.spc.js
+++ test/integration/amqp/rpc.spc.js
@@ -3,6 +3,7 @@
 const Promise = require("bluebird");
 const { ServiceBroker } = require("../../..");
 let { extendExpect, protectReject } = require("../../unit/utils");
+const purge = require("./purge");
 
 extendExpect(expect);
 
@@ -22,225 +23,259 @@ function createNode(name, disableBalancer = false, service) {
 	return broker;
 }
 
-describe("Test AMQPTransporter RPC with built-in balancer", () => {
+describe("Test AMQPTransporter", () => {
 
-	const client = createNode("client", false);
+	beforeAll(() => purge(purgeList));
+	afterAll(() => purge(purgeList));
 
-	const worker1 = createNode("worker1", false, {
-		name: "test",
-		actions: {
-			hello() {
-				return Promise.resolve(`Hello from ${this.broker.nodeID}`);
+	describe("Test AMQPTransporter RPC with built-in balancer", () => {
+
+		const client = createNode("client", false);
+
+		const worker1 = createNode("worker1", false, {
+			name: "test",
+			actions: {
+				hello() {
+					return Promise.resolve(`Hello from ${this.broker.nodeID}`);
+				}
 			}
-		}
-	});
-
-	const worker2 = createNode("worker2", false, {
-		name: "test",
-		actions: {
-			hello() {
-				return Promise.resolve(`Hello from ${this.broker.nodeID}`).delay(200);
-			}
-		}
-	});
-
-	const worker3 = createNode("worker3", false, {
-		name: "test",
-		actions: {
-			hello() {
-				return Promise.resolve(`Hello from ${this.broker.nodeID}`);
-			}
-		}
-	});
-
-	beforeEach(() => {
-		return Promise.all([
-			client.start(),
-			worker1.start(),
-			worker2.start(),
-			worker3.start(),
-		]);
-	});
-
-	afterEach(() => Promise.all([
-		client.stop(),
-		worker1.stop(),
-		worker2.stop(),
-		worker3.stop(),
-	]));
-
-	it("Should send an event to all subscribed nodes.", () => {
-		return Promise.delay(2000).then(() => {
-			return Promise.all([
-				client.call("test.hello"),
-				client.call("test.hello"),
-				client.call("test.hello"),
-				client.call("test.hello"),
-				client.call("test.hello"),
-				client.call("test.hello"),
-			]).catch(protectReject).then(res => {
-				//console.log(res);
-				expect(res).toHaveLength(6);
-				expect(res).toEqual(expect.arrayContaining([
-					"Hello from rpc-worker1",
-					"Hello from rpc-worker2",
-					"Hello from rpc-worker3",
-				]));
-				expect(res.filter(n => n == "Hello from rpc-worker1")).toHaveLength(2);
-				expect(res.filter(n => n == "Hello from rpc-worker2")).toHaveLength(2);
-				expect(res.filter(n => n == "Hello from rpc-worker3")).toHaveLength(2);
-			});
 		});
-	}, 10000);
-});
 
-describe("Test AMQPTransporter RPC with DISABLED built-in balancer", () => {
-
-	const client = createNode("client", true);
-
-	const worker1 = createNode("worker1", true, {
-		name: "test",
-		actions: {
-			hello() {
-				return Promise.resolve(`Hello from ${this.broker.nodeID}`);
+		const worker2 = createNode("worker2", false, {
+			name: "test",
+			actions: {
+				hello() {
+					return Promise.resolve(`Hello from ${this.broker.nodeID}`).delay(200);
+				}
 			}
-		}
-	});
-
-	const worker2 = createNode("worker2", true, {
-		name: "test",
-		actions: {
-			hello() {
-				return Promise.resolve(`Hello from ${this.broker.nodeID}`).delay(200);
-			}
-		}
-	});
-
-	const worker3 = createNode("worker3", true, {
-		name: "test",
-		actions: {
-			hello() {
-				return Promise.resolve(`Hello from ${this.broker.nodeID}`);
-			}
-		}
-	});
-
-	beforeEach(() => {
-		return Promise.all([
-			client.start(),
-			worker1.start(),
-			worker2.start(),
-			worker3.start(),
-		]);
-	});
-
-	afterEach(() => Promise.all([
-		client.stop(),
-		worker1.stop(),
-		worker2.stop(),
-		worker3.stop(),
-	]));
-
-	it("Should use availability-based load balancing", () => {
-		return Promise.delay(2000).then(() => {
-			return Promise.all([
-				client.call("test.hello"),
-				client.call("test.hello"),
-				client.call("test.hello"),
-				client.call("test.hello"),
-				client.call("test.hello"),
-				client.call("test.hello"),
-			]).catch(protectReject).then(res => {
-				console.log(res);
-				expect(res).toHaveLength(6);
-				expect(res).toEqual(expect.arrayContaining([
-					"Hello from rpc-worker1",
-					"Hello from rpc-worker2",
-					"Hello from rpc-worker3",
-				]));
-
-				// worker2 is slow, so it received only 1 request.
-				expect(res.filter(n => n == "Hello from rpc-worker2")).toHaveLength(1);
-			});
 		});
-	}, 10000);
-});
 
-describe.only("Test AMQPTransporter RPC with DISABLED built-in balancer & retried request", () => {
+		const worker3 = createNode("worker3", false, {
+			name: "test",
+			actions: {
+				hello() {
+					return Promise.resolve(`Hello from ${this.broker.nodeID}`);
+				}
+			}
+		});
 
-	const client = createNode("client", true);
-	let worker2CB = jest.fn();
+		beforeEach(() => {
+			return Promise.all([
+				client.start(),
+				worker1.start(),
+				worker2.start(),
+				worker3.start(),
+			]);
+		});
 
-	const worker1 = createNode("worker1", true, {
-		name: "test",
-		actions: {
-			hello(ctx) {
-				return Promise.resolve({
-					worker: "worker1",
-					a: ctx.params.a
+		afterEach(() => Promise.all([
+			client.stop(),
+			worker1.stop(),
+			worker2.stop(),
+			worker3.stop(),
+		]));
+
+		it("Should send an event to all subscribed nodes.", () => {
+			return Promise.delay(2000).then(() => {
+				return Promise.all([
+					client.call("test.hello"),
+					client.call("test.hello"),
+					client.call("test.hello"),
+					client.call("test.hello"),
+					client.call("test.hello"),
+					client.call("test.hello"),
+				]).catch(protectReject).then(res => {
+					//console.log(res);
+					expect(res).toHaveLength(6);
+					expect(res).toEqual(expect.arrayContaining([
+						"Hello from rpc-worker1",
+						"Hello from rpc-worker2",
+						"Hello from rpc-worker3",
+					]));
+					expect(res.filter(n => n == "Hello from rpc-worker1")).toHaveLength(2);
+					expect(res.filter(n => n == "Hello from rpc-worker2")).toHaveLength(2);
+					expect(res.filter(n => n == "Hello from rpc-worker3")).toHaveLength(2);
 				});
-			}
-		}
-	});
-
-	const worker2 = createNode("worker2", true, {
-		name: "test",
-		actions: {
-			hello() {
-				worker2CB();
-				return worker2.stop();
-				//return Promise.resolve(`Hello from ${this.broker.nodeID}`).delay(200);
-			}
-		}
-	});
-
-	const worker3 = createNode("worker3", true, {
-		name: "test",
-		actions: {
-			hello(ctx) {
-				return Promise.resolve({
-					worker: "worker3",
-					a: ctx.params.a
-				});
-			}
-		}
-	});
-
-	beforeEach(() => {
-		return Promise.all([
-			client.start(),
-			worker1.start(),
-			worker2.start(),
-			worker3.start(),
-		]);
-	});
-
-	afterEach(() => Promise.all([
-		client.stop(),
-		worker1.stop(),
-		//worker2.stop(),
-		worker3.stop(),
-	]));
-
-	it("should retry unacked requests to other node", () => {
-		return Promise.delay(2000).then(() => {
-			return Promise.all([
-				client.call("test.hello", { a: 1 }),
-				client.call("test.hello", { a: 2 }),
-				client.call("test.hello", { a: 3 }),
-				client.call("test.hello", { a: 4 }),
-				client.call("test.hello", { a: 5 }),
-				client.call("test.hello", { a: 6 }),
-			]).catch(protectReject).then(res => {
-				//console.log(res);
-				expect(res).toHaveLength(6);
-				expect(worker2CB).toHaveBeenCalledTimes(1);
-				expect(res.filter(o => o.worker == "worker1").length).toBeGreaterThan(0);
-				expect(res.filter(o => o.worker == "worker3").length).toBeGreaterThan(0);
-				// worker2 is crashed, so we didn't receive response from it.
-				expect(res.filter(o => o.worker == "worker2")).toHaveLength(0);
-				expect(res.map(o => o.a)).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6]));
 			});
+		}, 10000);
+	});
+
+	describe("Test AMQPTransporter RPC with DISABLED built-in balancer", () => {
+
+		const client = createNode("client", true);
+
+		const worker1 = createNode("worker1", true, {
+			name: "test",
+			actions: {
+				hello() {
+					return Promise.resolve(`Hello from ${this.broker.nodeID}`);
+				}
+			}
 		});
-	}, 10000);
+
+		const worker2 = createNode("worker2", true, {
+			name: "test",
+			actions: {
+				hello() {
+					return Promise.resolve(`Hello from ${this.broker.nodeID}`).delay(200);
+				}
+			}
+		});
+
+		const worker3 = createNode("worker3", true, {
+			name: "test",
+			actions: {
+				hello() {
+					return Promise.resolve(`Hello from ${this.broker.nodeID}`);
+				}
+			}
+		});
+
+		beforeEach(() => {
+			return Promise.all([
+				client.start(),
+				worker1.start(),
+				worker2.start(),
+				worker3.start(),
+			]);
+		});
+
+		afterEach(() => Promise.all([
+			client.stop(),
+			worker1.stop(),
+			worker2.stop(),
+			worker3.stop(),
+		]));
+
+		it("Should use availability-based load balancing", () => {
+			return Promise.delay(2000).then(() => {
+				return Promise.all([
+					client.call("test.hello"),
+					client.call("test.hello"),
+					client.call("test.hello"),
+					client.call("test.hello"),
+					client.call("test.hello"),
+					client.call("test.hello"),
+				]).catch(protectReject).then(res => {
+					//console.log(res);
+					expect(res).toHaveLength(6);
+					expect(res).toEqual(expect.arrayContaining([
+						"Hello from rpc-worker1",
+						"Hello from rpc-worker2",
+						"Hello from rpc-worker3",
+					]));
+
+					// worker2 is slow, so it received only 1 request.
+					expect(res.filter(n => n == "Hello from rpc-worker2")).toHaveLength(1);
+				});
+			});
+		}, 10000);
+	});
+
+	describe("Test AMQPTransporter RPC with DISABLED built-in balancer & retried request", () => {
+
+		const client = createNode("client", true);
+		let worker2CB = jest.fn();
+
+		const worker1 = createNode("worker1", true, {
+			name: "test",
+			actions: {
+				hello(ctx) {
+					return Promise.resolve({
+						worker: "worker1",
+						a: ctx.params.a
+					});
+				}
+			}
+		});
+
+		const worker2 = createNode("worker2", true, {
+			name: "test",
+			actions: {
+				hello() {
+					worker2CB();
+					return worker2.stop();
+					//return Promise.resolve(`Hello from ${this.broker.nodeID}`).delay(200);
+				}
+			}
+		});
+
+		const worker3 = createNode("worker3", true, {
+			name: "test",
+			actions: {
+				hello(ctx) {
+					return Promise.resolve({
+						worker: "worker3",
+						a: ctx.params.a
+					});
+				}
+			}
+		});
+
+		beforeEach(() => {
+			return Promise.all([
+				client.start(),
+				worker1.start(),
+				worker2.start(),
+				worker3.start(),
+			]);
+		});
+
+		afterEach(() => Promise.all([
+			client.stop(),
+			worker1.stop(),
+			//worker2.stop(),
+			worker3.stop(),
+		]));
+
+		it("should retry unacked requests to other node", () => {
+			return Promise.delay(2000).then(() => {
+				return Promise.all([
+					client.call("test.hello", { a: 1 }),
+					client.call("test.hello", { a: 2 }),
+					client.call("test.hello", { a: 3 }),
+					client.call("test.hello", { a: 4 }),
+					client.call("test.hello", { a: 5 }),
+					client.call("test.hello", { a: 6 }),
+				]).catch(protectReject).then(res => {
+					//console.log(res);
+					expect(res).toHaveLength(6);
+					expect(worker2CB).toHaveBeenCalledTimes(1);
+					expect(res.filter(o => o.worker == "worker1").length).toBeGreaterThan(0);
+					expect(res.filter(o => o.worker == "worker3").length).toBeGreaterThan(0);
+					// worker2 is crashed, so we didn't receive response from it.
+					expect(res.filter(o => o.worker == "worker2")).toHaveLength(0);
+					expect(res.map(o => o.a)).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6]));
+				});
+			});
+		}, 10000);
+	});
+
 });
+
+const purgeList = {
+	queues: [
+		"MOL-test-rpc.REQ.rpc-client",
+		"MOL-test-rpc.REQ.rpc-worker1",
+		"MOL-test-rpc.REQ.rpc-worker2",
+		"MOL-test-rpc.REQ.rpc-worker3",
+		"MOL-test-rpc.REQB.$node.actions",
+		"MOL-test-rpc.REQB.$node.events",
+		"MOL-test-rpc.REQB.$node.health",
+		"MOL-test-rpc.REQB.$node.list",
+		"MOL-test-rpc.REQB.$node.services",
+		"MOL-test-rpc.REQB.test.hello",
+		"MOL-test-rpc.RES.rpc-client",
+		"MOL-test-rpc.RES.rpc-worker1",
+		"MOL-test-rpc.RES.rpc-worker2",
+		"MOL-test-rpc.RES.rpc-worker3",
+	],
+
+	exchanges: [
+		"MOL-test-rpc.DISCONNECT",
+		"MOL-test-rpc.DISCOVER",
+		"MOL-test-rpc.HEARTBEAT",
+		"MOL-test-rpc.INFO",
+		"MOL-test-rpc.PING",
+	]
+};
