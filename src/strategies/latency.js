ed8003032db66fe62c334a8938eba4c174cec572
Rachel Chen <me@rachelchen.me>
2018-03-27 12:07:30 -0700

    self-enclosed strategy module

---
+++ src/strategies/latency.js
@@ -0,0 +1,231 @@
+/*
+ * moleculer
+ * Copyright (c) 2018 MoleculerJS (https://github.com/moleculerjs/moleculer)
+ * MIT Licensed
+ */
+"use strict";
+
+const _ = require("lodash");
+
+const { random } = require("lodash");
+const BaseStrategy = require("./base");
+
+/**
+ * Lowest latency invocation strategy
+ *
+ * Since Strategy can be instantiated multiple times, therefore,
+ * we need to have a "master" instance to send ping, and each
+ * individual "slave" instance will update their list dynamically
+ *
+ * These options can be configured in broker registry options:
+ *
+ * const broker = new ServiceBroker({
+ * 	logger: true,
+ * 	registry: {
+ * 		strategy: "LatencyStrategy",
+ * 		strategyOptions: {
+ * 			sampleCount: 5,
+ * 			lowLatency: 10,
+ * 			collectCount: 5,
+ * 			pingInterval: 10
+ * 		}
+ * 	}
+ * });
+ *
+ * @class LatencyStrategy
+ */
+class LatencyStrategy extends BaseStrategy {
+
+	constructor(registry, broker) {
+		super(registry, broker);
+
+		this.opts = _.defaultsDeep(registry.opts.strategyOptions, {
+			sampleCount: 5,
+			lowLatency: 10,
+			collectCount: 5,
+			pingInterval: 10
+		});
+
+		this.brokerStopped = false;
+
+		this.hostLatency = new Map();
+
+		/* hostMap contains:
+			hostname => {
+				historicLatency: [],
+				nodeList: []
+			}
+		*/
+		this.hostMap = new Map();
+
+		// short circuit
+		if (!this.broker.transit) return;
+
+		if (this.broker.localBus.listenerCount("$node.latencyMaster") === 0) {
+			// claim as master
+			this.broker.localBus.on("$node.latencyMaster", function() {});
+			// respond to PONG
+			this.broker.localBus.on("$node.pong", this.processPong.bind(this));
+			// dynamically add new node
+			this.broker.localBus.on("$node.connected", this.addNode.bind(this));
+			// dynamically remove node
+			this.broker.localBus.on("$node.disconnected", this.removeHostMap.bind(this));
+			// try to discovery all nodes on start up
+			this.broker.localBus.on("$broker.started", this.discovery.bind(this));
+			// clean up ourselves
+			this.broker.localBus.on("$broker.stopped", () => this.brokerStopped = true);
+		} else {
+			// remove node if we are told by master
+			this.broker.localBus.on("$node.latencySlave.removeHost", this.removeHostLatency.bind(this));
+		}
+
+		this.broker.localBus.on("$node.latencySlave", this.updateLatency.bind(this));
+	}
+
+	// Master
+	discovery() {
+		return this.broker.transit.sendPing().then(function() {
+			setTimeout(this.pingHosts.bind(this), 1000 * this.opts.pingInterval);
+		}.bind(this));
+	}
+
+	// Master
+	pingHosts() {
+
+		if (this.brokerStopped) return;
+		/*
+			Smart Ping: only ping the host, not the nodes (which may be many)
+
+			Although, if that particular node on the host is overloaded,
+			the measurement may be skewed.
+		*/
+		let hosts = [];
+		if (this.hostMap.size > 0) {
+			hosts = this.hostMap.values();
+		}
+
+		this.broker.Promise.map(hosts, function(host) {
+			return this.broker.transit.sendPing(host.nodeList[0]);
+		}.bind(this), { concurrency: 5 }).then(function() {
+			setTimeout(this.pingHosts.bind(this), 1000 * this.opts.pingInterval);
+		}.bind(this));
+	}
+
+	// Master
+	processPong(payload) {
+		let node = this.registry.nodes.get(payload.nodeID);
+		if (!node) return;
+
+		let avgLatency = null;
+
+		this.mapNode(node);
+
+		let hostMap = this.hostMap.get(node.hostname);
+
+		if (hostMap.historicLatency.length > (this.opts.collectCount - 1))
+			hostMap.historicLatency.shift();
+
+		hostMap.historicLatency.push(payload.elapsedTime);
+
+		avgLatency = hostMap.historicLatency.reduce(function(sum, latency) {
+			return sum + latency;
+		}, 0) / hostMap.historicLatency.length;
+
+		this.broker.localBus.emit("$node.latencySlave", {
+			hostname: node.hostname,
+			avgLatency: avgLatency
+		});
+	}
+
+	// Master
+	mapNode(node) {
+		if (typeof this.hostMap.get(node.hostname) === "undefined") {
+			this.hostMap.set(node.hostname, {
+				historicLatency: [],
+				nodeList: [ node.id ]
+			});
+		}
+	}
+
+	// Master
+	addNode(payload) {
+		let node = payload.node;
+
+		this.mapNode(node);
+		// each host may have multiple nodes
+		let hostMap = this.hostMap.get(node.hostname);
+		if (hostMap.nodeList.indexOf(node.id) === -1) {
+			hostMap.nodeList.push(node.id);
+		}
+	}
+
+	// Master
+	removeHostMap(payload) {
+		let node = payload.node;
+
+		let hostMap = this.hostMap.get(node.hostname);
+		if (typeof hostMap === "undefined") return;
+
+		let nodeIndex = hostMap.nodeList.indexOf(node.id);
+		if (nodeIndex > -1) {
+			hostMap.nodeList.splice(nodeIndex, 1);
+		}
+
+		if (hostMap.nodeList.length > 0) return;
+
+		// only remove the host if the last node disconnected
+
+		this.broker.localBus.emit("$node.latencySlave.removeHost", node.hostname);
+		this.hostMap.delete(node.hostname);
+	}
+
+	// Slave
+	updateLatency(payload) {
+		this.hostLatency.set(payload.hostname, payload.avgLatency);
+	}
+
+	// Slave
+	removeHostLatency(hostname) {
+		this.hostLatency.delete(hostname);
+	}
+
+	select(list) {
+		let minEp = null;
+		let minLatency = null;
+
+		const sampleCount = this.opts.sampleCount;
+		const count = sampleCount <= 0 || sampleCount > list.length ? list.length : sampleCount;
+		for (let i = 0; i < count; i++) {
+			let ep;
+			// Get random endpoint
+			if (count == list.length) {
+				ep = list[i];
+			} else {
+				ep = list[random(0, list.length - 1)];
+			}
+			const epLatency = this.hostLatency.get(ep.node.hostname);
+
+			// Check latency of endpoint
+			if (typeof epLatency !== "undefined") {
+
+				if (epLatency < this.opts.lowLatency)
+					return ep;
+
+				if (!minEp || !minLatency || epLatency < minLatency) {
+					minLatency = epLatency;
+					minEp = ep;
+				}
+			}
+		}
+
+		// Return the lowest latency
+		if (minEp) {
+			return minEp;
+		}
+
+		// Return a random item (no latency data)
+		return list[random(0, list.length - 1)];
+	}
+}
+
+module.exports = LatencyStrategy;
---
+++ test/unit/strategies/latency.spec.js
@@ -0,0 +1,300 @@
+"use strict";
+
+let LatencyStrategy = require("../../../src/strategies/latency");
+let { extendExpect } = require("../utils");
+const ServiceBroker = require("../../../src/service-broker");
+const Promise = require("bluebird");
+
+extendExpect(expect);
+
+describe("Test LatencyStrategy", () => {
+
+	it("should have properties", () => {
+
+		const brokerWithTransit = new ServiceBroker({ nodeID: "node-1", transporter: "fake" });
+
+		let strategy = new LatencyStrategy({
+			opts: {
+				strategyOptions: {}
+			}
+		}, brokerWithTransit);
+
+		expect(brokerWithTransit.localBus.listenerCount("$broker.started")).toBe(2);
+		expect(brokerWithTransit.localBus.listenerCount("$node.latencyMaster")).toBe(1);
+		expect(brokerWithTransit.localBus.listenerCount("$node.latencySlave")).toBe(1);
+		expect(brokerWithTransit.localBus.listenerCount("$node.pong")).toBe(1);
+		expect(brokerWithTransit.localBus.listenerCount("$node.connected")).toBe(1);
+		expect(brokerWithTransit.localBus.listenerCount("$node.disconnected")).toBe(1);
+
+		return brokerWithTransit.start().then(() => {
+			expect(strategy.hostMap.size).toBe(0);
+			expect(strategy.hostLatency.size).toBe(0);
+		}).then(() => {
+			return brokerWithTransit.stop();
+		});
+	});
+
+	it("test without options", () => {
+
+		const brokerWithNoTransit = new ServiceBroker();
+
+		let strategy = new LatencyStrategy({
+			opts: {
+				strategyOptions: {}
+			}
+		}, brokerWithNoTransit);
+
+		expect(strategy.opts.pingInterval).toBe(10);
+		expect(strategy.opts.sampleCount).toBe(5);
+		expect(strategy.opts.lowLatency).toBe(10);
+		expect(strategy.opts.collectCount).toBe(5);
+	});
+
+	it("test with options", () => {
+
+		const brokerWithNoTransit = new ServiceBroker();
+
+		let strategy = new LatencyStrategy({
+			opts: {
+				strategyOptions: {
+		            sampleCount: 15,
+		            lowLatency: 20,
+		            collectCount: 10,
+		            pingInterval: 15
+				}
+			}
+		}, brokerWithNoTransit);
+
+		expect(strategy.opts.pingInterval).toBe(15);
+		expect(strategy.opts.sampleCount).toBe(15);
+		expect(strategy.opts.lowLatency).toBe(20);
+		expect(strategy.opts.collectCount).toBe(10);
+	});
+
+	it("test without latency data", () => {
+
+		const brokerWithNoTransit = new ServiceBroker();
+
+		let strategy = new LatencyStrategy({
+			opts: {
+				strategyOptions: {}
+			}
+		}, brokerWithNoTransit);
+
+		const list = [
+			{ a: "hello", node: { id: 'a' } },
+			{ b: "world", node: { id: 'b' } },
+			{ b: "now", node: { id: 'c' } }
+		];
+
+		return brokerWithNoTransit.start().then(() => {
+			expect(strategy.select(list)).toBeAnyOf(list);
+			expect(strategy.select(list)).toBeAnyOf(list);
+			expect(strategy.select(list)).toBeAnyOf(list);
+		});
+
+	});
+
+	it("test with latency data (where node has a low latency)", () => {
+
+		const brokerWithNoTransit = new ServiceBroker();
+
+		let strategy = new LatencyStrategy({
+			opts: {
+				strategyOptions: {}
+			}
+		}, brokerWithNoTransit);
+
+		strategy.hostLatency.set("a", 5);
+		strategy.hostLatency.set("b", 20);
+		strategy.hostLatency.set("c", 100);
+
+		const list = [
+			{ a: "hello", node: { hostname: "a" } },
+			{ b: "world", node: { hostname: 'b' } },
+			{ b: "now", node: { hostname: "c" } }
+		];
+
+		expect(strategy.select(list)).toBe(list[0]);
+		expect(strategy.select(list)).toBe(list[0]);
+		expect(strategy.select(list)).toBe(list[0]);
+
+	});
+
+	it("test with latency data (where all nodes have some latency)", () => {
+
+		const brokerWithNoTransit = new ServiceBroker();
+
+		let strategy = new LatencyStrategy({
+			opts: {
+				strategyOptions: {}
+			}
+		}, brokerWithNoTransit);
+
+		strategy.hostLatency.set("a", 50);
+		strategy.hostLatency.set("b", 20);
+		strategy.hostLatency.set("c", 100);
+		strategy.hostLatency.set("d", 1000);
+
+		const list = [
+			{ a: "hello", node: { hostname: "a" } },
+			{ b: "world", node: { hostname: 'b' } },
+			{ b: "now", node: { hostname: "c" } }
+		];
+
+		expect(strategy.select(list)).toBe(list[1]);
+		expect(strategy.select(list)).toBe(list[1]);
+		expect(strategy.select(list)).toBe(list[1]);
+
+	});
+
+	it("test with latency data (where all we have lots of nodes)", () => {
+
+		const brokerWithNoTransit = new ServiceBroker();
+
+		let strategy = new LatencyStrategy({
+			opts: {
+				strategyOptions: {}
+			}
+		}, brokerWithNoTransit);
+
+		strategy.hostLatency.set("a", 50);
+		strategy.hostLatency.set("b", 20);
+		strategy.hostLatency.set("c", 100);
+		strategy.hostLatency.set("d", 1000);
+
+		const list = [
+			{ a: "hello", node: { hostname: "a" } },
+			{ b: "world", node: { hostname: 'b' } },
+			{ b: "12345", node: { hostname: "c" } },
+			{ a: "olleh", node: { hostname: "a" } },
+			{ b: "dlorw", node: { hostname: 'b' } },
+			{ b: "54321", node: { hostname: "c" } },
+			{ a: "ooooo", node: { hostname: "a" } },
+			{ b: "ppppp", node: { hostname: 'b' } },
+			{ b: "wwwww", node: { hostname: "c" } },
+			{ a: "aaaaa", node: { hostname: "a" } },
+			{ b: "bbbbb", node: { hostname: 'b' } },
+			{ b: "ccccc", node: { hostname: "c" } }
+		];
+
+		expect(strategy.select(list)).toBeAnyOf(list);
+		expect(strategy.select(list)).toBeAnyOf(list);
+		expect(strategy.select(list)).toBeAnyOf(list);
+
+	});
+
+	it("test with three nodes", () => {
+		let A = new ServiceBroker({
+			nodeID: "node-A",
+			transporter: "fake",
+			registry: {
+		        strategy: 'Latency',
+				preferLocal: false,
+		        strategyOptions: {
+		            pingInterval: 1
+		        }
+		    }
+		});
+
+		A.createService({
+			name: 'Svc',
+			actions: {
+				echo: function(ctx) {
+					return "A";
+				}
+			}
+		});
+
+		let B = new ServiceBroker({
+			nodeID: "node-B",
+			transporter: "fake",
+			registry: {
+		        strategy: 'Latency',
+				preferLocal: false,
+		        strategyOptions: {
+		            pingInterval: 1
+		        }
+		    }
+		});
+
+		B.createService({
+			name: 'Svc',
+			actions: {
+				echo: function(ctx) {
+					return "B";
+				}
+			}
+		});
+
+		let C = new ServiceBroker({
+			nodeID: "node-C",
+			transporter: "fake",
+			registry: {
+		        strategy: 'Latency',
+				preferLocal: false,
+		        strategyOptions: {
+		            pingInterval: 1
+		        }
+		    }
+		});
+
+		C.createService({
+			name: 'Svc',
+			actions: {
+				echo: function(ctx) {
+					return "C";
+				}
+			}
+		});
+
+		let caller = new ServiceBroker({
+			nodeID: "node-caller",
+			transporter: "fake",
+			registry: {
+				strategy: 'Latency',
+				preferLocal: false,
+				strategyOptions: {
+					pingInterval: 1
+				}
+			}
+		});
+
+		return Promise.all([
+			A.start().delay(500),
+			B.start().delay(500),
+			C.start().delay(500),
+			caller.start().delay(500)
+		])
+		.then(function() {
+			return Promise.map(new Array(10), () => {
+				return caller.call('Svc.echo').then((res) => {
+					expect(["A", "B", "C"]).toContain(res);
+				});
+			})
+		})
+		.then(() => B.stop())
+		.then(function() {
+			return Promise.map(new Array(10), () => {
+				return caller.call('Svc.echo').then((res) => {
+					expect(["A", "C"]).toContain(res);
+				});
+			})
+		})
+		.then(() => A.stop())
+		.then(function() {
+			return Promise.map(new Array(10), () => {
+				return caller.call('Svc.echo').then((res) => {
+					expect(["C"]).toContain(res);
+				});
+			})
+		})
+		.then(() => {
+			return Promise.all([
+				C.stop(),
+				caller.stop()
+			])
+		})
+	});
+
+});
