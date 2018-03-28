54dcd1f8b6fb44e53aba2598f764c8041db18a36
Rachel Chen <me@rachelchen.me>
2018-03-27 22:24:55 -0700

    new ping strategy: host based instead of node based

--- src/strategies/latency.js
+++ src/strategies/latency.js
@@ -46,73 +46,164 @@ class LatencyStrategy extends BaseStrategy {
 			pingInterval: 10
 		});
 
-		// Master
-		this.historicLatency = Object.create(null);
-		// Slave
-		this.nodeLatency = Object.create(null);
+		this.hostLatency = new Map();
 
 		// short circuit
 		if (!this.broker.transit) return;
 
 		if (this.broker.localBus.listenerCount("$node.latencyMaster") === 0) {
-			//this.broker.logger.debug("Latency: We are MASTER");
+			this.broker.logger.debug("Latency: We are MASTER");
+
+			/* nodeMap contains:
+				nodeID => {
+					historicLatency: [],
+					hostname: null
+				}
+			*/
+			this.nodeMap = new Map();
+
+			/* hostMap contains:
+				hostname => {
+					nodeID: null,
+					nodeCounter: 1
+				}
+			*/
+			this.hostMap = new Map();
+
+			// claim as master
 			this.broker.localBus.on("$node.latencyMaster", function() {});
+			// respond to PONG
 			this.broker.localBus.on("$node.pong", this.processPong.bind(this));
-			this.broker.localBus.on("$broker.started", this.sendPing.bind(this));
+			// dynamically add new node
+			this.broker.localBus.on("$node.connected", this.addNode.bind(this));
+			// dynamically remove node
+			this.broker.localBus.on("$node.disconnected", this.removeHostMap.bind(this));
+			// try to discovery all nodes on start up
+			this.broker.localBus.on("$broker.started", this.discovery.bind(this));
 		} else {
-			//this.broker.logger.debug("Latency: We are SLAVE");
+			this.broker.logger.debug("Latency: We are SLAVE");
+			// remove node if we are told by master
+			this.broker.localBus.on("$node.latencySlave.removeHost", this.removeHostLatency.bind(this));
 		}
 
 		this.broker.localBus.on("$node.latencySlave", this.updateLatency.bind(this));
-		this.broker.localBus.on("$node.disconnected", this.cleanUp.bind(this));
 	}
 
 	// Master
-	sendPing() {
-		//this.broker.logger.debug("Latency: Sending ping");
-		this.broker.transit.sendPing().then(function() {
-			setTimeout(this.sendPing.bind(this), 1000 * this.opts.pingInterval);
+	discovery() {
+		return this.broker.transit.sendPing().then(function() {
+			setTimeout(this.pingHosts.bind(this), 1000 * this.opts.pingInterval);
+		}.bind(this));
+	}
+
+	// Master
+	pingHosts() {
+		this.broker.logger.debug("Latency: Sending ping to hosts");
+		/*
+			Smart Ping: only ping the host, not the nodes (which may be many)
+
+			Although, if that particular node on the host is overloaded,
+			the measurement may be skewed.
+		*/
+		let hosts = [];
+		if (this.hostMap.size > 0) {
+			hosts = this.hostMap.values();
+		}
+
+		this.broker.Promise.map(hosts, function(host) {
+			this.broker.logger.debug("Latency: Sending ping to", host.nodeID);
+			return this.broker.transit.sendPing(host.nodeID);
+		}.bind(this), { concurrency: 5 }).then(function() {
+			setTimeout(this.pingHosts.bind(this), 1000 * this.opts.pingInterval);
 		}.bind(this));
 	}
 
 	// Master
 	processPong(payload) {
-		let nodeID = payload.nodeID;
+		let node = this.registry.nodes.get(payload.nodeID)
+		if (!node) return;
+
 		let avgLatency = null;
 
-		//this.broker.logger.debug("Latency: Process incoming pong");
+		this.broker.logger.debug("Latency: Process incoming pong");
 
-		if (typeof this.historicLatency[nodeID] === "undefined")
-			this.historicLatency[nodeID] = [];
+		this.mapNode(node);
 
-		if (this.historicLatency[nodeID].length > (this.opts.collectCount - 1))
-			this.historicLatency[nodeID].shift();
+		let nodeMap = this.nodeMap.get(node.id);
 
-		this.historicLatency[nodeID].push(payload.elapsedTime);
+		if (nodeMap.historicLatency.length > (this.opts.collectCount - 1))
+			nodeMap.historicLatency.shift();
 
-		avgLatency = this.historicLatency[nodeID].reduce(function(sum, latency) {
+		nodeMap.historicLatency.push(payload.elapsedTime);
+
+		avgLatency = nodeMap.historicLatency.reduce(function(sum, latency) {
 			return sum + latency;
-		}, 0) / this.historicLatency[nodeID].length;
+		}, 0) / nodeMap.historicLatency.length;
 
-		//this.broker.logger.debug("Latency: Broadcasting latency update");
+		this.broker.logger.debug("Latency: Broadcasting latency update");
 
 		this.broker.localBus.emit("$node.latencySlave", {
-			nodeID: nodeID,
+			hostname: node.hostname,
 			avgLatency: avgLatency
 		});
 	}
 
-	// Slave
-	updateLatency(payload) {
-		//this.broker.logger.debug("Latency update received", payload);
-		this.nodeLatency[payload.nodeID] = payload.avgLatency;
+	// Master
+	mapNode(node) {
+		if (typeof this.nodeMap.get(node.id) === "undefined") {
+			this.nodeMap.set(node.id, {
+				historicLatency: [],
+				hostname: node.hostname
+			});
+		}
+
+		if (typeof this.hostMap.get(node.hostname) === "undefined") {
+			this.hostMap.set(node.hostname, {
+				nodeID: node.id,
+				nodeCounter: 1
+			});
+		}
 	}
 
-	// Master & Slave
-	cleanUp(payload) {
-		//this.broker.logger.debug("Deleting historic latency", payload.node.id);
-		delete this.historicLatency[payload.node.id];
-		delete this.nodeLatency[payload.node.id];
+	// Master
+	addNode(payload) {
+
+		this.broker.logger.debug("Latency: adding new node");
+
+		let node = payload.node;
+
+		this.mapNode(node);
+		// each host may have multiple nodes
+		this.hostMap.get(node.hostname).nodeCounter++;
+
+		this.broker.logger.debug("Latency: ", node.hostname, "has", this.hostMap.get(node.hostname).nodeCounter, "nodes");
+	}
+
+	// Master
+	removeHostMap(payload) {
+		let node = payload.node;
+
+		this.hostMap.get(node.hostname).nodeCounter--;
+		if (this.hostMap.get(node.hostname).nodeCounter > 1) return;
+
+		// only remove the host if the last node disconnected
+
+		this.broker.logger.debug("Latency: removing host", node.hostname);
+
+		this.broker.localBus.emit("$node.latencySlave.removeHost", node.hostname);
+		this.nodeMap.delete(node.id);
+		this.hostMap.delete(node.hostname);
+	}
+
+	// Slave
+	updateLatency(payload) {
+		this.broker.logger.debug("Latency update received", payload);
+		this.hostLatency.set(payload.hostname, payload.avgLatency);
+	}
+
+	// Slave
+	removeHostLatency(hostname) {
+		this.hostLatency.delete(hostname);
 	}
 
 	select(list) {
@@ -129,7 +220,7 @@ class LatencyStrategy extends BaseStrategy {
 			} else {
 				ep = list[random(0, list.length - 1)];
 			}
-			const epLatency = this.nodeLatency[ep.node.id];
+			const epLatency = this.hostLatency.get(ep.node.hostname);
 
 			// Check latency of endpoint
 			if (typeof epLatency !== "undefined") {
@@ -146,11 +237,11 @@ class LatencyStrategy extends BaseStrategy {
 
 		// Return the lowest latency
 		if (minEp) {
-			//this.broker.logger.debug("Latency: Select", minEp.node.id, minLatency);
+			this.broker.logger.debug("Latency: Select", minEp.node.hostname, minLatency);
 			return minEp;
 		}
 
-		//this.broker.logger.debug("Latency: Select random");
+		this.broker.logger.debug("Latency: Select random");
 
 		// Return a random item (no latency data)
 		return list[random(0, list.length - 1)];
