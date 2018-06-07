e66ff5a6c262e895d96531cd6a91f7e454479d4b
Icebob <mereg.norbert@gmail.com>
2018-06-07 14:27:29 +0200

    create stat.service from expanded stat module

--- dev/dev.js
+++ dev/dev.js
@@ -6,8 +6,9 @@ const util = require("util");
 const broker = new ServiceBroker({
 	nodeID: "dev-" + process.pid,
 	transporter: "NATS",
+	metrics: true,
 	//logLevel: "debug",
-	logObjectPrinter: o => util.inspect(o, { depth: 2, colors: true, breakLength: 100 }), // `breakLength: 50` activates multi-line object
+	logObjectPrinter: o => util.inspect(o, { depth: 4, colors: true, breakLength: 50 }), // `breakLength: 50` activates multi-line object
 });
 
 broker.createService({
@@ -21,10 +22,11 @@ broker.createService({
 	}
 });
 
+broker.loadService("./examples/stat.service.js");
 
 broker.start()
 	.then(() => broker.repl())
 	.delay(1000)
-	.then(() => broker.call("$node.health"))
+	.then(() => broker.call("stat.snapshot"))
 	.then(res => broker.logger.info(res))
 	.catch(err => broker.logger.error(err));
