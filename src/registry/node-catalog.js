95ccd269133ceec1d4c3746cea8f1fe4b6f817ba
fugufish <therealfugu@gmail.com>
2018-08-17 06:42:59 -0600

    remove extra hash

--- src/registry/node-catalog.js
+++ src/registry/node-catalog.js
@@ -61,7 +61,7 @@ class NodeCatalog {
 
 		}, this.broker.options.heartbeatInterval * 1000);
 
-		if (typeof this.heartbeatTimer === "number") throw new Error("heartbeatTimer should not be a number. See issue [##362] for details.");
+		if (typeof this.heartbeatTimer === "number") throw new Error("heartbeatTimer should not be a number. See issue [#362] for details.");
 
 
 		this.heartbeatTimer.unref();
