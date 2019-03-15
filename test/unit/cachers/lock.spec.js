99fa4610bb44b70f33fa89cd6683b5b2c621e2a8
tiaod <tiaod@qq.com>
2019-03-15 22:09:19 +0800

    Update test

--- test/unit/cachers/lock.spec.js
+++ test/unit/cachers/lock.spec.js
@@ -15,21 +15,22 @@ describe("Test lock method", () => {
 			cacher._lock = jest.fn((key, callback)=>{
 				let done = (cb)=>{
 					unlockFn = jest.fn(()=>{
-						cb(err)
-					})
-					return unlockFn
+						cb(err);
+					});
+					return unlockFn;
 				}
-				callback(done)
-			})
+				callback(done);
+			});
 
 			return cacher.lock('key').then(unlock => {
 				unlock().catch(e=>{
 					expect(unlockFn).toHaveBeenCalledTimes(1);
 					expect(e).toBe(err);
-				})
-			})
-		}))
-	})
+				});
+			});
+		}));
+	});
+
 	it("should lock the concurrency call", () => {
 		return Promise.all(cachers.map(Cacher => {
       const cacher = new Cacher({
@@ -63,10 +64,29 @@ describe("Test lock method", () => {
     }));
 	});
 
-})
+});
 
 describe("Test tryLock method", () => {
 	let cachers = [MemoryCacher, MemoryLRUCacher]
+
+	it("should able to lock", ()=> {
+		return Promise.all(cachers.map(Cacher => {
+			const cacher = new Cacher({
+				ttl: 30,
+				lock: true
+			});
+			const broker = new ServiceBroker({
+				logger: false,
+				cacher
+			});
+			const key = '123fff'
+			const tryLock = jest.spyOn(cacher, 'tryLock')
+			return cacher.tryLock(key).then(unlock => {
+				expect(cacher._lock.isLocked(key + '-lock')).toBeTruthy()
+				return unlock()
+			});
+		}))
+	})
 
 	it("should throw an error when already locked.", ()=>{
 		return Promise.all(cachers.map(Cacher => {
@@ -81,11 +101,38 @@ describe("Test tryLock method", () => {
 			const key = '123fff'
 			const tryLock = jest.spyOn(cacher, 'tryLock')
 			return cacher.tryLock(key).then(unlock => {
-				return cacher.tryLock().catch(e => {
+				return cacher.tryLock(key).catch(e => {
 					expect(e.message).toEqual('Locked.')
-				})
-			})
-		}))
-	})
+				});
+			});
+		}));
+	});
 
-})
+	it("should reject the promise when failed to unlock", ()=>{
+		return Promise.all(cachers.map(Cacher => {
+			const cacher = new Cacher({
+				ttl:30,
+				lock: true
+			})
+			let err = new Error('Can not acquire a lock.')
+			let unlockFn;
+			cacher._lock = jest.fn((key, callback)=>{
+				let done = (cb)=>{
+					unlockFn = jest.fn(()=>{
+						cb(err);
+					})
+					return unlockFn;
+				}
+				callback(done);
+			})
+			cacher._lock.isLocked = jest.fn( key => false )
+
+			return cacher.tryLock('key').then(unlock => {
+				unlock().catch(e=>{
+					expect(unlockFn).toHaveBeenCalledTimes(1);
+					expect(e).toBe(err);
+				});
+			});
+		}));
+	});
+});
