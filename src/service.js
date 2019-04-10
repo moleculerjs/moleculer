07e414321fafc55d9485c5d9d725da5523d90ad7
Andrei Kazakou <andrei.k@geomotiv.com>
2019-04-10 12:00:58 +0300

    #483: Revert changes in service.js

--- src/service.js
+++ src/service.js
@@ -39,11 +39,11 @@ class Service {
 	 * Creates an instance of Service by schema.
 	 *
 	 * @param {ServiceBroker} 	broker	broker of service
-	 * @param {Object=} 		schema	schema of service
+	 * @param {Object} 			schema	schema of service
 	 *
 	 * @memberof Service
 	 */
-	constructor(broker, schema = undefined) {
+	constructor(broker, schema) {
 		if (!_.isObject(broker))
 			throw new ServiceSchemaError("Must set a ServiceBroker instance!");
 
