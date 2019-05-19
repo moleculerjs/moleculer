2156c87a14e7cb932f04dd4a14cbd0cc87247b8f
Icebob <mereg.norbert@gmail.com>
2019-05-19 23:03:44 +0200

    fix log level in CB

--- src/middlewares/circuit-breaker.js
+++ src/middlewares/circuit-breaker.js
@@ -134,7 +134,7 @@ module.exports = function() {
 		item.cbTimer.unref();
 
 		const rate = item.count > 0 ? item.failures / item.count : 0;
-		logger.info(`Circuit breaker has been opened on '${item.ep.name}' endpoint.`, { nodeID: item.ep.id, action: item.ep.action.name, failures: item.failures, count: item.count, rate });
+		logger.debug(`Circuit breaker has been opened on '${item.ep.name}' endpoint.`, { nodeID: item.ep.id, action: item.ep.action.name, failures: item.failures, count: item.count, rate });
 		broker.broadcast("$circuit-breaker.opened", { nodeID: item.ep.id, action: item.ep.action.name, failures: item.failures, count: item.count, rate });
 	}
 
@@ -185,7 +185,7 @@ module.exports = function() {
 		item.failures = 0;
 		item.count = 0;
 
-		logger.info(`Circuit breaker has been closed on '${item.ep.name}' endpoint.`, { nodeID: item.ep.id, action: item.ep.action.name });
+		logger.debug(`Circuit breaker has been closed on '${item.ep.name}' endpoint.`, { nodeID: item.ep.id, action: item.ep.action.name });
 
 		broker.broadcast("$circuit-breaker.closed", { nodeID: item.ep.id, action: item.ep.action.name });
 
