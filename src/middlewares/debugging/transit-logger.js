942a27f58c8ffbe5f144d198abac9cc8a53a8301
Icebob <mereg.norbert@gmail.com>
2019-05-22 22:54:55 +0200

    polishing transit logger

--- src/middlewares/debugging/transit-logger.js
+++ src/middlewares/debugging/transit-logger.js
@@ -17,12 +17,15 @@ module.exports = function TransitLoggerMiddleware(opts) {
 		logger: null,
 		logLevel: "info",
 		logPacketData: false,
+
 		folder: null,
+		extension: ".json",
+
 		colors: {
 			receive: "grey",
 			send: "grey"
 		},
-		extension: ".json",
+
 		packetFilter: ["HEARTBEAT"]
 	});
 
@@ -32,11 +35,14 @@ module.exports = function TransitLoggerMiddleware(opts) {
 	let targetFolder;
 
 	function saveToFile(filename, payload) {
-		fs.writeFile(path.join(targetFolder, filename), JSON.stringify(payload, null, 4), () => { /* Silent error */ });
+		const data = JSON.stringify(payload, payload instanceof Error ? Object.getOwnPropertyNames(payload) : null, 4);
+		fs.writeFile(path.join(targetFolder, filename), data, () => { /* Silent error */ });
 	}
 
 	const coloringSend = opts.colors && opts.colors.send ? opts.colors.send.split(".").reduce((a,b) => a[b], chalk) : s => s;
 	const coloringReceive = opts.colors && opts.colors.receive ? opts.colors.receive.split(".").reduce((a,b) => a[b], chalk) : s => s;
+
+	let logFn;
 
 	return {
 		created(broker) {
@@ -47,24 +53,30 @@ module.exports = function TransitLoggerMiddleware(opts) {
 				targetFolder = path.join(opts.folder, nodeID);
 				makeDirs(targetFolder);
 			}
+
+			logFn = opts.logLevel ? logger[opts.logLevel] : null;
 		},
 
 		transitPublish(next) {
 			return packet => {
+				// Packet filtering
 				if (opts.packetFilter.includes(packet.type)) {
 					return next(packet);
 				}
 
 				const payload = packet.payload;
 
-				if (opts.logLevel) {
-					logger[opts.logLevel](coloringSend(`=> Send ${packet.type} packet to '${packet.target || "<all nodes>"}'`));
-					if (opts.logPacketData)
-						logger[opts.logLevel]("=>", payload);
+				// Logging to logger
+				if (logFn) {
+					logFn(coloringSend(`=> Send ${packet.type} packet to '${packet.target || "<all nodes>"}'`));
+					if (opts.logPacketData) {
+						logFn("=>", payload);
+					}
 				}
 
-				if (targetFolder)
+				if (targetFolder) {
 					saveToFile(`${Date.now()}-send-${packet.type}-to-${packet.target || "all"}${opts.extension}`, payload);
+				}
 
 				return next(packet);
 			};
@@ -72,6 +84,7 @@ module.exports = function TransitLoggerMiddleware(opts) {
 
 		transitMessageHandler(next) {
 			return (cmd, packet) => {
+				// Packet filtering
 				if (opts.packetFilter.includes(cmd)) {
 					return next(cmd, packet);
 				}
@@ -79,13 +92,15 @@ module.exports = function TransitLoggerMiddleware(opts) {
 				const payload = packet.payload;
 
 				if (opts.logLevel) {
-					logger[opts.logLevel](coloringReceive(`<= Receive ${cmd} packet from '${payload.sender}'`));
-					if (opts.logPacketData)
-						logger[opts.logLevel]("<=", packet.payload);
+					logFn(coloringReceive(`<= Receive ${cmd} packet from '${payload.sender}'`));
+					if (opts.logPacketData) {
+						logFn("<=", packet.payload);
+					}
 				}
 
-				if (targetFolder)
+				if (targetFolder) {
 					saveToFile(`${Date.now()}-receive-${cmd}-from-${payload.sender}${opts.extension}`, payload);
+				}
 
 				return next(cmd, packet);
 			};
