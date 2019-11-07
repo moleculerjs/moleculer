0d475a68fe4c82a721091c0c610dd958530406a6
Icebob <mereg.norbert@gmail.com>
2019-11-07 21:13:37 +0100

    update changelog

--- examples/runner/moleculer.config.async.js
+++ examples/runner/moleculer.config.async.js
@@ -1,24 +1,21 @@
 "use strict";
 
 /*
-	This is an async external configuration file for Moleculer Runner
+	This is an async external configuration file (downloaded via HTTP request) for Moleculer Runner
 
 	Start Broker
 
 		Windows:
-			node bin\moleculer-runner.js -c examples\runner\moleculer.config.async.js -r examples/user.service.js
+			node bin\moleculer-runner.js -c examples/runner/moleculer.config.async.js -r examples/user.service.js
 
 		Linux:
 			node ./bin/moleculer-runner -c examples/runner/moleculer.config.async.js -r examples/user.service.js
 
 */
 
+const fetch = require("node-fetch");
+
 module.exports = async function() {
-	return {
-		namespace: "bbb",
-		nodeID: "async-config-node",
-		logger: true,
-		logLevel: "debug",
-		//transporter: "TCP"
-	};
+	const res = await fetch("https://pastebin.com/raw/SLZRqfHX");
+	return await res.json();
 };
