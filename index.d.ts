879edca558664fa73348593dd5a96009a484ca01
Glenn Ryan <gpr@storis.com>
2019-11-29 10:54:52 -0500

    Added null possibility to meta - the method has a condition for expecting meta to be null - this condition warrants the possibility to expect null as meta

--- index.d.ts
+++ index.d.ts
@@ -1051,7 +1051,7 @@ declare namespace Moleculer {
 			del(key: string|Array<string>): PromiseLike<any>;
 			clean(match?: string|Array<string>): PromiseLike<any>;
 			getCacheKey(actionName: string, params: object, meta: object, keys: Array<string> | null) : string;
-			defaultKeygen(actionName: string, params: object | null, meta: object, keys: Array<string> | null): string;
+			defaultKeygen(actionName: string, params: object | null, meta: object | null, keys: Array<string> | null): string;
 		}
 
 		class Memory extends Base {
