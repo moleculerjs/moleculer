484635811b41065b73ac0fe082f7423df59ecc32
AndreMaz <andre.maz.90@gmail.com>
2019-12-16 22:17:05 +0000

    Fix test

--- test/unit/transit.spec.js
+++ test/unit/transit.spec.js
@@ -2064,7 +2064,7 @@ describe("Test Transit.sendNodeInfo", () => {
 		});
 	});
 
-	it("should call publish with correct params if has no nodeID & disableBalancer: true", () => {
+	it("should call publish with correct params if has no nodeID", () => {
 		// Set disableBalancer option
 		broker.options.disableBalancer = true
 		transit.publish.mockClear();
@@ -2072,33 +2072,6 @@ describe("Test Transit.sendNodeInfo", () => {
 
 		return transit.sendNodeInfo().then(() => {
 			expect(transit.tx.makeBalancedSubscriptions).toHaveBeenCalledTimes(1);
-			expect(transit.publish).toHaveBeenCalledTimes(1);
-			expect(broker.getLocalNodeInfo).toHaveBeenCalledTimes(1);
-			const packet = transit.publish.mock.calls[0][0];
-			expect(packet).toBeInstanceOf(P.Packet);
-			expect(packet.type).toBe(P.PACKET_INFO);
-			expect(packet.target).toBe();
-			expect(packet.payload).toEqual({
-				"client": undefined,
-				"config": undefined,
-				"hostname": undefined,
-				"instanceID": broker.instanceID,
-				"ipList": undefined,
-				"metadata": { "region": "eu-west1" },
-				"seq": undefined,
-				"services": []
-			});
-		});
-	});
-
-	it("should call publish with correct params if has no nodeID & disableBalancer: false", () => {
-		// Set disableBalancer option
-		broker.options.disableBalancer = false
-		transit.publish.mockClear();
-		broker.getLocalNodeInfo.mockClear();
-
-		return transit.sendNodeInfo().then(() => {
-			expect(transit.tx.makeBalancedSubscriptions).toHaveBeenCalledTimes(0);
 			expect(transit.publish).toHaveBeenCalledTimes(1);
 			expect(broker.getLocalNodeInfo).toHaveBeenCalledTimes(1);
 			const packet = transit.publish.mock.calls[0][0];
