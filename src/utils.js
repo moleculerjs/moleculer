167ebaeb830fd8d071088fee0be500c92aa2b24b
Icebob <mereg.norbert@gmail.com>
2020-01-06 15:51:43 +0100

    add utils.parseByteString

--- src/utils.js
+++ src/utils.js
@@ -20,6 +20,16 @@ const RegexCache = new Map();
 
 const deprecateList = [];
 
+const byteMultipliers = {
+	b:  1,
+	kb: 1 << 10,
+	mb: 1 << 20,
+	gb: 1 << 30,
+	tb: Math.pow(1024, 4),
+	pb: Math.pow(1024, 5),
+};
+const parseByteStringRe = /^((-|\+)?(\d+(?:\.\d+)?)) *(kb|mb|gb|tb|pb)$/i;
+
 class TimeoutError extends Error {
 
 }
@@ -301,6 +311,43 @@ const utils = {
 				}
 				return currentPath;
 			}, "");
+	},
+
+	/**
+	 * Parse a byte string to number of bytes. E.g "1kb" -> 1024
+	 * Credits: https://github.com/visionmedia/bytes.js
+	 *
+	 * @param {String} v
+	 * @returns {Number}
+	 */
+	parseByteString(v) {
+		if (typeof v === "number" && !isNaN(v)) {
+			return v;
+		}
+
+		if (typeof v !== "string") {
+			return null;
+		}
+
+		// Test if the string passed is valid
+		let results = parseByteStringRe.exec(v);
+		let floatValue;
+		let unit = "b";
+
+		if (!results) {
+			// Nothing could be extracted from the given string
+			floatValue = parseInt(v, 10);
+			if (Number.isNaN(floatValue))
+				return null;
+
+			unit = "b";
+		} else {
+			// Retrieve the value and the unit
+			floatValue = parseFloat(results[1]);
+			unit = results[4].toLowerCase();
+		}
+
+		return Math.floor(byteMultipliers[unit] * floatValue);
 	}
 };
 
