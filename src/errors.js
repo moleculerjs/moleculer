2d6af1065af71a545ad5d4015e75cbf5b1626ecb
Icebob <mereg.norbert@gmail.com>
2021-09-05 18:28:50 +0200

    remove es6-error dependency, npm audit

--- src/errors.js
+++ src/errors.js
@@ -6,7 +6,43 @@
 
 "use strict";
 
-const ExtendableError = require("es6-error");
+/**
+ * Extendable errors class.
+ *
+ * Credits: https://github.com/bjyoungblood/es6-error/blob/master/src/index.js
+ */
+class ExtendableError extends Error {
+	constructor(message = "") {
+		super(message);
+
+		// extending Error is weird and does not propagate `message`
+		Object.defineProperty(this, "message", {
+			configurable: true,
+			enumerable: false,
+			value: message,
+			writable: true
+		});
+
+		Object.defineProperty(this, "name", {
+			configurable: true,
+			enumerable: false,
+			value: this.constructor.name,
+			writable: true
+		});
+
+		if (Object.prototype.hasOwnProperty.call(Error, "captureStackTrace")) {
+			Error.captureStackTrace(this, this.constructor);
+			return;
+		}
+
+		Object.defineProperty(this, "stack", {
+			configurable: true,
+			enumerable: false,
+			value: new Error(message).stack,
+			writable: true
+		});
+	}
+}
 
 /**
  * Custom Moleculer Error class
