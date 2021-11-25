dbe61b4b314e1c9f43f1406646e23e5a5c8b907f
AndreMaz <andre.maz.90@gmail.com>
2021-11-25 08:39:38 +0000

    update example

--- dev/issue1036.js
+++ dev/issue1036.js
@@ -1,80 +1,78 @@
 const ServiceBroker = require("../src/service-broker");
-const BaseStrategy = require('../index').Strategies.Base
+const { Strategies } = require("../index");
 
+const { random } = require("lodash");
 
-class CustomStrategy extends BaseStrategy {
-    select(list, ctx) {
-        console.log('====> CUSTOM <=====')
-        return list[random(0, list.length - 1)];
-    }
+// Create custom strategy
+class CustomStrategy extends Strategies.Base {
+	select(list, ctx) {
+		console.log("====> CUSTOM <=====");
+		return list[random(0, list.length - 1)];
+	}
 }
+
+// Register custom strategy
+Strategies.register("Custom", CustomStrategy);
 
 function createBroker(nodeID, strategy, svcSchema) {
-    
-    const broker = new ServiceBroker({
-        nodeID: nodeID,
-        transporter: "NATS",
-    
-        registry: {
-            strategy: strategy,
-        },
-    });
+	const broker = new ServiceBroker({
+		nodeID: nodeID,
+		transporter: "NATS",
 
-    broker.createService(svcSchema)
+		registry: {
+			strategy: strategy
+		}
+	});
 
-    return broker
+	broker.createService(svcSchema);
+
+	return broker;
 }
-
-
 
 const schema1 = {
-    name: "test",
-    actions: {
-        first(ctx) {
-            return `[${this.broker.nodeID}] Expected to use: RoundRobin, it works!`
-        },
-        second: {
-            strategy: "RoundRobin",
-            handler () {
-                return `[${this.broker.nodeID}] Expected to use: CustomStrategy, still RoundRobin in use..`
-            }
-        },
-    }
-}
+	name: "test",
+	actions: {
+		first(ctx) {
+			return `[${this.broker.nodeID}] Expected to use: RoundRobin, it works!`;
+		},
+		second: {
+			strategy: "RoundRobin",
+			handler() {
+				return `[${this.broker.nodeID}] Expected to use: CustomStrategy, still RoundRobin in use..`;
+			}
+		}
+	}
+};
 
 const schema2 = {
-    name: "custom",
-    actions: {
-        first(ctx) {
-            return `[${this.broker.nodeID}] "Expected to use: RoundRobin, it works!"`
-        },
-        second: {
-            strategy: CustomStrategy,
-            handler () {
-                return `[${this.broker.nodeID}] Expected to use: CustomStrategy, it works!`
-            }
-        },
-    }
-}
+	name: "custom",
+	actions: {
+		first(ctx) {
+			return `[${this.broker.nodeID}] "Expected to use: RoundRobin, it works!"`;
+		},
+		second: {
+			// Set custom strategy for this action
+			strategy: "Custom",
+			handler() {
+				return `[${this.broker.nodeID}] Expected to use: CustomStrategy, it works!`;
+			}
+		}
+	}
+};
 
-const brokerRoundRobinBase1 = createBroker('roundRobin1', 'RoundRobin', schema1)
+const brokerRoundRobinBase1 = createBroker("roundRobin1", "RoundRobin", schema1);
 
-const brokerCustomBase1 = createBroker('brokerCustomBase1', CustomStrategy, schema2)
-const brokerCustomBase2 = createBroker('brokerCustomBase2', CustomStrategy, schema2)
-
-
-
-
+const brokerCustomBase1 = createBroker("brokerCustomBase1", CustomStrategy, schema2);
+const brokerCustomBase2 = createBroker("brokerCustomBase2", CustomStrategy, schema2);
 
 Promise.all([
-    brokerRoundRobinBase1.start(),
-    brokerCustomBase1.start(),
-    brokerCustomBase2.start(),
-    ]).then(async () => {
+	brokerRoundRobinBase1.start(),
+	brokerCustomBase1.start(),
+	brokerCustomBase2.start()
+]).then(async () => {
+	brokerRoundRobinBase1.repl();
 
-    brokerRoundRobinBase1.repl()
-
-    console.log('Waiting...')
-    await brokerRoundRobinBase1.Promise.delay(1500)
-    console.log('Waiting done...')
-})
+	console.log("Waiting for other brokers...");
+	await brokerRoundRobinBase1.Promise.delay(1500);
+	console.log("Waiting done...");
+});
