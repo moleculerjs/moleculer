8dd4214cc61a6573e756ceb45b2475aa465445fb
AndreMaz <andre.maz.90@gmail.com>
2022-02-23 19:21:57 +0000

    update repl types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 import { EventEmitter2 } from "eventemitter2";
+import { REPLServer } from "repl";
 
 declare namespace Moleculer {
 	/**
@@ -991,12 +992,6 @@ declare namespace Moleculer {
 		version?: string|number;
 	}
 
-	interface MoleculerRepl extends Vorpal{
-		removeIfExist(command:string): void;
-	}
-
-
-
 	namespace Loggers {
 		class Base {
 			constructor(opts?: GenericObject);
@@ -1047,7 +1042,7 @@ declare namespace Moleculer {
 		start(): Promise<void>;
 		stop(): Promise<void>;
 
-		repl(): MoleculerRepl;
+		repl(): REPLServer;
 
 		errorHandler(err: Error, info: GenericObject): void;
 
@@ -1536,85 +1531,6 @@ declare namespace Moleculer {
 	const CIRCUIT_CLOSE: string;
 	const CIRCUIT_HALF_OPEN: string;
 	const CIRCUIT_OPEN: string;
-
-	class Vorpal {
-		parse(argv: ReadonlyArray<string>): this;
-		delimiter(value: string): this;
-		show(): this;
-		hide(): this;
-		find(command: string): Vorpal.Command;
-		exec(command: string): Promise<{}>;
-		execSync(command: string): Promise<{}>;
-		log(value: string, ...values: string[]): this;
-		history(id: string): this;
-		localStorage(id: string): object;
-		help(value: (cmd: string) => string): this;
-		pipe(value: (stdout: string) => string): this;
-		use(extension: Vorpal.Extension): this;
-		catch(command: string, description?: string): Vorpal.Catch;
-		command(command: string, description?: string): Vorpal.Command;
-		version(version: string): this;
-		sigint(value: () => void): this;
-		ui: Vorpal.UI;
-		activeCommand: Vorpal.CommandInstance;
-	}
-
-	namespace Vorpal {
-		interface Args {
-			[key: string]: any;
-			options: {
-				[key: string]: any;
-			};
-		}
-
-		interface PromptObject {
-			[key: string]: any;
-		}
-
-		type Action = (args: Args) => Promise<void>;
-		type Cancel = () => void;
-
-		class Command {
-			_name: string;
-			_fn: Action;
-			_cancel: Cancel | undefined;
-			alias(command: string): this;
-			parse(value: (command: string, args: Args) => string): this;
-			option(option: string, description: string, autocomplete?: ReadonlyArray<string>): this;
-			types(types: { string?: ReadonlyArray<string> }): this;
-			hidden(): this;
-			remove(): this;
-			help(value: (args: Args) => void): this;
-			validate(value: (args: Args) => boolean | string): this;
-			autocomplete(values: ReadonlyArray<string> | { data: () => Promise<ReadonlyArray<string>> }): this;
-			action(action: Action): this;
-			cancel(cancel: Cancel): this;
-			allowUnknownOptions(): this;
-		}
-
-		class Catch extends Command { }
-
-		class Extension { }
-
-		class UI {
-			delimiter(text?: string): string;
-			input(text?: string): string;
-			imprint(): void;
-			submit(command: string): string;
-			cancel(): void;
-			redraw: {
-				(text: string, ...texts: string[]): void;
-				clear(): void;
-				done(): void;
-			};
-		}
-
-		class CommandInstance {
-			log(value: string, ...values: string[]): void;
-			prompt(prompt: object | ReadonlyArray<object>): Promise<PromptObject>;
-			delimiter(value: string): void;
-		}
-	}
 
 	namespace Utils {
 		function isFunction(func: unknown): func is Function;
