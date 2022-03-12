9f6bc2b62f19f3be974d52fea183e1fb76d49d0a
Jarod <therealfugu@gmail.com>
2022-03-11 20:59:43 -0700

    add replCommands to brokerOptions

--- index.d.ts
+++ index.d.ts
@@ -810,6 +810,16 @@ declare namespace Moleculer {
 		options?: GenericObject
 	}
 
+	interface ReplCommand {
+		command: string;
+		description: string;
+		alias?: string;
+		options: Array<object>,
+		types: object,
+		allowUnknownOptions?: boolean;
+		action: (broker: ServiceBroker, opts: any) => any;
+	}
+
 	interface BrokerOptions {
 		namespace?: string;
 		nodeID?: string;
@@ -878,6 +888,8 @@ declare namespace Moleculer {
 		skipProcessEventRegistration?: boolean;
 
 		maxSafeObjectSize?: number;
+
+		replCommands?: Array<ReplCommand>;
 	}
 
 	interface NodeHealthStatus {
