6be1f7d35c498697e4c6b34784803accb1b4757f
0x0a0d <vandug23@gmail.com>
2022-03-30 16:52:59 +0700

    d.ts: allow custom ctx.locals type

--- index.d.ts
+++ index.d.ts
@@ -521,7 +521,7 @@ declare namespace Moleculer {
 		disconnected(): void;
 	}
 
-	class Context<P = unknown, M extends object = {}, L extends object = {}> {
+	class Context<P = unknown, M extends object = {}, L extends object = GenericObject> {
 		constructor(broker: ServiceBroker, endpoint: Endpoint);
 		id: string;
 		broker: ServiceBroker;
