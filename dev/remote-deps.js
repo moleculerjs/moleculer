37d72b12d8a23884f9072204fad7ac06ce44982f
AndreMaz <andre.maz.90@gmail.com>
2022-04-05 15:28:10 +0100

    add repro example

---
+++ dev/remote-deps.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const ServiceBroker = require("../src/service-broker");
+
+const broker1 = new ServiceBroker({
+	nodeID: "node-1",
+
+	transporter: "Redis"
+});
+
+const broker2 = new ServiceBroker({
+	nodeID: "node-2",
+
+	transporter: "Redis"
+});
+
+const locationSchema = {
+	name: "location",
+
+	// depends on device.service at node-2
+	dependencies: ["device"],
+
+	async started() {
+		this.logger.info("Location Services started");
+	}
+};
+
+const tenantSchema = {
+	name: "tenant",
+
+	async started() {
+		this.logger.info("Tenant Services started");
+	}
+};
+
+const assetSchema = {
+	name: "device",
+
+	// Depends on tenant.service located at node-1
+	dependencies: ["tenant"],
+
+	async started() {
+		this.logger.info("Device Services started");
+	}
+};
+
+// Place location.service and tenant.service at node-1
+broker1.createService(locationSchema);
+broker1.createService(tenantSchema);
+
+// Place asset.service at node-2
+broker2.createService(assetSchema);
+
+Promise.all([broker1.start(), broker2.start()])
+	.then(() => {
+		broker1.repl();
+	})
+	.catch(err => console.log(err));
