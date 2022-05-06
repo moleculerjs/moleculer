fc314cfffde4e5291bfd1c3b200089946bcbcd6d
intech <ru31337@gmail.com>
2022-05-06 14:01:52 +0400

    fix: legacy support node v10

--- src/registry/event-catalog.js
+++ src/registry/event-catalog.js
@@ -183,7 +183,7 @@ class EventCatalog {
 		);
 
 		if (isBroadcast) {
-			promises = _.flatMap(filteredEvents,list => {
+			promises = _.flatMap(filteredEvents, list => {
 				return list.endpoints.map(ep => {
 					if (ep.local && ep.event.handler) {
 						const newCtx = ctx.copy(ep);
