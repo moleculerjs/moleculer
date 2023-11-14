24d3851fc7b1412597aa703fe085e9fda78e0480
Kovács Lajos <lajoskvcs@icloud.com>
2023-11-14 01:31:17 +0100

    fix: fixed custom logger passing

--- src/loggers/index.js
+++ src/loggers/index.js
@@ -6,7 +6,7 @@
 
 "use strict";
 
-const { isObject, isString, isInheritedClass } = require("../utils");
+const { isObject, isString, isInheritedClass, isFunction } = require("../utils");
 const { BrokerOptionsError } = require("../errors");
 const Base = require("./base");
 
@@ -42,7 +42,7 @@ function getByName(name) {
  * @memberof ServiceBroker
  */
 function resolve(opt) {
-	if (isObject(opt) && isInheritedClass(opt, Loggers.Base)) {
+	if (isFunction(opt) && isInheritedClass(opt, Loggers.Base)) {
 		return opt;
 	} else if (isString(opt)) {
 		let LoggerClass = getByName(opt);
