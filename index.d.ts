1fe737d0d0b83d3ba45aeedabfba13275cf33e24
Icebob <mereg.norbert@gmail.com>
2023-12-10 17:38:15 +0100

    rewrite index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,12 @@
 // --- SERVICE BROKER ---
 
 import ServiceBroker = require("./src/service-broker");
-export { ServiceBroker };
-export type { BrokerOptions, CallingOptions } from "./src/service-broker";
+import type { BrokerOptions, CallingOptions } from "./src/service-broker";
 
 // --- SERVICE ---
 
 import Service = require("./src/service");
-export { Service };
-export type {
+import type {
 	ActionHooks,
 	ActionHandler,
 	ActionParams,
@@ -24,98 +22,138 @@ export type {
 // --- CONTEXT ---
 
 import Context = require("./src/context");
-export { Context };
 
 // --- TRANSIT ---
 
 import Transit = require("./src/transit");
-export { Transit };
-export * as Packet from "./src/packets";
+import * as Packet from "./src/packets";
 
 // --- RUNNER ---
 
 import Runner = require("./src/runner");
-export { Runner };
-export type { RunnerFlags } from "./src/runner";
+import type { RunnerFlags } from "./src/runner";
 
 // --- ERRORS ---
 
-export * as Errors from "./src/errors";
+import * as Errors from "./src/errors";
 
 // --- UTILS ---
 
-export * as Utils from "./src/utils";
+import * as Utils from "./src/utils";
 
 // --- CONSTANTS ---
 
-export type {
+import type {
 	CIRCUIT_CLOSE,
 	CIRCUIT_HALF_OPEN,
 	CIRCUIT_HALF_OPEN_WAIT,
 	CIRCUIT_OPEN
 } from "./src/constants";
 
-export declare const MOLECULER_VERSION: string;
-export declare const PROTOCOL_VERSION: string;
-export declare const INTERNAL_MIDDLEWARES: string[];
-
 // --- CACHERS ---
 
-export * as Cachers from "./src/cachers";
+import * as Cachers from "./src/cachers";
 
 // --- LOGGERS ---
 
-export * as Loggers from "./src/loggers";
-export type { LogLevels } from "./src/loggers";
-export type { Logger, LoggerConfig } from "./src/logger-factory";
+import * as Loggers from "./src/loggers";
+import type { LogLevels } from "./src/loggers";
+import type { Logger, LoggerConfig } from "./src/logger-factory";
 
 // --- METRICS ---
 
-export * as MetricTypes from "./src/metrics/types";
-export * as MetricReporters from "./src/metrics/reporters";
+import * as MetricTypes from "./src/metrics/types";
+import * as MetricReporters from "./src/metrics/reporters";
 import MetricRegistry = require("./src/metrics/registry");
-export { MetricRegistry };
-export * as METRIC from "./src/metrics/constants";
+import * as METRIC from "./src/metrics/constants";
 
 // --- MIDDLEWARES ---
 
-export type { CallMiddlewareHandler, Middleware } from "./src/middleware";
+import type { CallMiddlewareHandler, Middleware } from "./src/middleware";
 
 // --- SERVICE REGISTRY ---
 
 import Registry = require("./src/registry");
-export { Registry };
 
 import type EndpointList = require("./src/registry/endpoint-list");
-export type { EndpointList };
 import type Endpoint = require("./src/registry/endpoint");
-export type { Endpoint };
 import type ActionEndpoint = require("./src/registry/endpoint-action");
-export type { ActionEndpoint };
 import type EventEndpoint = require("./src/registry/endpoint-event");
-import { Packet } from "./src/packets";
-export type { EventEndpoint };
 
-export * as Discoverers from "./src/registry/discoverers";
+import * as Discoverers from "./src/registry/discoverers";
 
 // --- SERIALIZERS ---
 
-export * as Serializers from "./src/serializers";
+import * as Serializers from "./src/serializers";
 
 // --- STRATEGIES ---
 
-export * as Strategies from "./src/strategies";
+import * as Strategies from "./src/strategies";
 
 // --- TRACING ---
 
-export type { Tracer, Span, TracerOptions } from "./src/tracing";
-export * as TracerExporters from "./src/tracing/exporters";
+import type { Tracer, Span, TracerOptions } from "./src/tracing";
+import * as TracerExporters from "./src/tracing/exporters";
 
 // --- TRANSPORTERS ---
 
-export * as Transporters from "./src/transporters";
+import * as Transporters from "./src/transporters";
 
 // --- VALIDATORS ---
 
-export * as Validators from "./src/validators";
-export type { ValidatorNames } from "./src/validators";
+import * as Validators from "./src/validators";
+import type { ValidatorNames } from "./src/validators";
+
+declare namespace Moleculer {
+	export { ServiceBroker, BrokerOptions, CallingOptions };
+
+	export {
+		Service,
+		ActionHooks,
+		ActionHandler,
+		ActionParams,
+		ActionVisibility,
+		ServiceHooks,
+		ServiceHooksAfter,
+		ServiceSchema,
+		ServiceSettingSchema,
+		ServiceAction,
+		ServiceActions
+	};
+
+	export { Context };
+
+	export { Transit, Packet };
+
+	export { Runner, RunnerFlags };
+
+	export { Errors };
+
+	export { Utils };
+
+	export { CIRCUIT_CLOSE, CIRCUIT_HALF_OPEN, CIRCUIT_HALF_OPEN_WAIT, CIRCUIT_OPEN };
+
+	export const MOLECULER_VERSION: string;
+	export const PROTOCOL_VERSION: string;
+	export const INTERNAL_MIDDLEWARES: string[];
+
+	export { Cachers };
+
+	export { Loggers, Logger, LoggerConfig, LogLevels };
+
+	export { MetricTypes, MetricReporters, MetricRegistry, METRIC };
+
+	export { CallMiddlewareHandler, Middleware };
+
+	export { Registry, Discoverers, EndpointList, Endpoint, ActionEndpoint, EventEndpoint };
+
+	export { Serializers };
+
+	export { Strategies };
+
+	export { Tracer, Span, TracerOptions, TracerExporters };
+
+	export { Transporters };
+
+	export { Validators, ValidatorNames };
+}
