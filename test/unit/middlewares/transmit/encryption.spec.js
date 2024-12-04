de52131927adbe92af6fd6e395227e9072d7721c
Icebob <mereg.norbert@gmail.com>
2024-12-04 20:43:08 +0100

    remove non-iv cipher tests

--- test/unit/middlewares/transmit/encryption.spec.js
+++ test/unit/middlewares/transmit/encryption.spec.js
@@ -13,23 +13,6 @@ describe("Test EncryptionMiddleware", () => {
 	});
 
 	it("should encrypt the data", () => {
-		const mw = Middleware(password);
-
-		const meta = {};
-		const next = jest.fn();
-		const send = mw.transporterSend.call(broker, next);
-		const encrypter = crypto.createCipher("aes-256-cbc", password);
-
-		send("topic", Buffer.from("plaintext data"), meta);
-
-		expect(next).toHaveBeenCalledTimes(1);
-		expect(next).toHaveBeenCalledWith("topic", expect.any(Buffer), meta);
-		expect(next.mock.calls[0][1]).toEqual(
-			Buffer.concat([encrypter.update("plaintext data"), encrypter.final()])
-		);
-	});
-
-	it("should encrypt the data with IV", () => {
 		const pass = crypto.randomBytes(32);
 		const iv = crypto.randomBytes(16);
 		const mw = Middleware(pass, "aes-256-ctr", iv);
@@ -46,23 +29,6 @@ describe("Test EncryptionMiddleware", () => {
 		expect(next.mock.calls[0][1]).toEqual(
 			Buffer.concat([encrypter.update("plaintext data"), encrypter.final()])
 		);
-	});
-
-	it("should decrypt data with IV", () => {
-		const mw = Middleware(password);
-
-		const meta = {};
-		const next = jest.fn();
-		const receive = mw.transporterReceive.call(broker, next);
-		const encrypter = crypto.createCipher("aes-256-cbc", password);
-		const encryptedData = Buffer.concat([
-			encrypter.update("plaintext data"),
-			encrypter.final()
-		]);
-
-		receive("topic", encryptedData, meta);
-		expect(next).toHaveBeenCalledTimes(1);
-		expect(next).toHaveBeenCalledWith("topic", Buffer.from("plaintext data"), meta);
 	});
 
 	it("should decrypt data", () => {
