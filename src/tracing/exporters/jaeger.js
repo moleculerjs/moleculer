3ffbe7f150c5f6fb31d44b5b2b52ff5dfe4dc64d
Icebob <mereg.norbert@gmail.com>
2025-01-09 09:40:04 +0000

    trigger actions

--- src/tracing/exporters/jaeger.js
+++ src/tracing/exporters/jaeger.js
@@ -1,6 +1,6 @@
 /*
  * moleculer
- * Copyright (c) 2020 MoleculerJS (https://github.com/moleculerjs/moleculer)
+ * Copyright (c) 2025 MoleculerJS (https://github.com/moleculerjs/moleculer)
  * MIT Licensed
  */
 
