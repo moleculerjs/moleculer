5d09c86b9d50d5c05ca81377029a01e54cd8a597
oscarsotoo1 <oscarjesussoto@gmail.com>
2025-10-08 12:56:24 -0700

    feat(testing): add testing-helpers package skeleton

---
+++ packages/testing-helpers/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Testing helpers - esqueleto inicial
+ * - export createBrokerHelper(...) 
+ * - export mockAction, mockEvent, etc.
+ */
+
+export function createBrokerHelper(opts?: any) {
+  // TODO: implementar
+  return {};
+}
